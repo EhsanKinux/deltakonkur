0a9afd596b4979864a1a994e4da846e41deef49a
EhsanKINUX <ehsankhwentao@gmail.com>
2024-05-28 12:10:17 +0330

    feat: create sidebar routes

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,13 @@ import SignIn from "./components/pages/auth/sign-in/SignIn.tsx";
 import SignUp from "./components/pages/auth/sign-up/SignUp.tsx";
 import DashboardLayout from "./components/pages/dashboard/dashboardLayout/DashboardLayout.tsx";
 import Dashboard from "./components/pages/dashboard/dashboard/Dashboard.tsx";
+import Reserve from "./components/pages/dashboard/dashboardPages/reserve/Reserve.tsx";
+import Advisors from "./components/pages/dashboard/dashboardPages/advisors/Advisors.tsx";
+import Accounting from "./components/pages/dashboard/dashboardPages/accounting/Accounting.tsx";
+import Supervision from "./components/pages/dashboard/dashboardPages/supervision/Supervision.tsx";
+import Canceling from "./components/pages/dashboard/dashboardPages/canceling/Canceling.tsx";
+import Content from "./components/pages/dashboard/dashboardPages/content/Content.tsx";
+import Users from "./components/pages/dashboard/dashboardPages/users/Users.tsx";
 
 const router = createBrowserRouter([
   {
@@ -37,6 +44,34 @@ const router = createBrowserRouter([
           {
             path: "",
             element: <Dashboard />,
+          },
+          {
+            path: "reserve",
+            element: <Reserve />,
+          },
+          {
+            path: "advisors",
+            element: <Advisors />,
+          },
+          {
+            path: "accounting",
+            element: <Accounting />,
+          },
+          {
+            path: "supervision",
+            element: <Supervision />,
+          },
+          {
+            path: "canceling",
+            element: <Canceling />,
+          },
+          {
+            path: "content",
+            element: <Content />,
+          },
+          {
+            path: "users",
+            element: <Users />,
           },
         ],
       },
