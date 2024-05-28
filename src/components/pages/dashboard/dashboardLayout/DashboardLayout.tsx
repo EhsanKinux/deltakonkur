fd81e66420c6879a01bf9856139ae3b9ca1640d6
EhsanKINUX <ehsankhwentao@gmail.com>
2024-05-28 12:10:48 +0330

    feat: add mobile and desktop sidebar

--- src/components/pages/dashboard/dashboardLayout/DashboardLayout.tsx
+++ src/components/pages/dashboard/dashboardLayout/DashboardLayout.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Sidebar from "./parts/sidebar/Sidebar";
+import MobileNav from "./parts/sidebar/MobileNav";
 
 const DashboardLayout = () => {
   return (
-    <div className="">
-      <nav className="">This is Dashboard Layout</nav>
-      <main className="font-bold bg-slate-200">
+    <main className="flex h-screen w-full font-bold bg-slate-200">
+      <Sidebar />
+      <div className="flex size-full flex-col">
+        <div className="sidebar-layout">
+          {/* <img src={hamIcon} width={30} height={30} alt="Menu" /> */}
+          <div>
+            <MobileNav />
+          </div>
+        </div>
         <Outlet />
-      </main>
-    </div>
+      </div>
+    </main>
   );
 };
 
