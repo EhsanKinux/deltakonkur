ce0eb811dbe86c175b791e6135a841d9be0d1d83
EhsanKINUX <ehsankhwentao@gmail.com>
2024-05-29 15:34:44 +0330

    fix: add ui to authentication layout

--- src/components/pages/auth/authLayout/AuthLayout.tsx
+++ src/components/pages/auth/authLayout/AuthLayout.tsx
@@ -3,8 +3,10 @@ import { Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
   return (
-    <div className="w-full flex flex-col justify-center items-center h-screen">
-      <main className="auth-form">
+    <div className="w-full min-h-screen flex flex-col justify-center items-center bg-slate-300">
+      <main className="auth-form bg-white p-8 md:p-12">
+        <div className="absolute -top-16 -left-16 w-72 h-72 bg-blue-400 rounded-full mix-blend-multiply filter blur-3xl opacity-70"></div>
+        <div className="absolute -bottom-16 -right-20 w-72 h-72 bg-pink-400 rounded-full mix-blend-multiply filter blur-3xl opacity-70"></div>
         <Outlet />
       </main>
     </div>
