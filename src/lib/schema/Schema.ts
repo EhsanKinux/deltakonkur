94eebbbdd3e6bf59680c0f9fdc30aa38a4a85c2c
EhsanKINUX <ehsankhwentao@gmail.com>
2024-05-29 15:42:29 +0330

    fix: change error messages

--- src/lib/schema/Schema.ts
+++ src/lib/schema/Schema.ts
@@ -2,6 +2,6 @@ import { z } from "zod";
 
 export const authFormSchema = (type) =>
   z.object({
-    tell: z.string().min(11).max(11),
-    password: z.string().min(8),
+    tell: z.string().min(11, { message: "نام کاربری اشتباه است" }).max(11, { message: "نام کاربری اشتباه است" }),
+    password: z.string().min(8, { message: "حداقل 8 کاراکتر باید وارد شود" }),
   });
