d9c497493993ac827be184af8ce6ad667d7f270f
EhsanKINUX <ehsankhwentao@gmail.com>
2024-05-29 20:11:55 +0330

    fix: add and creaete schema for reserve page form

--- src/lib/schema/Schema.ts
+++ src/lib/schema/Schema.ts
@@ -5,3 +5,15 @@ export const authFormSchema = (type) =>
     tell: z.string().min(11, { message: "نام کاربری اشتباه است" }).max(11, { message: "نام کاربری اشتباه است" }),
     password: z.string().min(8, { message: "حداقل 8 کاراکتر باید وارد شود" }),
   });
+
+export const registerFormSchema = () =>
+  z.object({
+    name: z.string(),
+    lastName: z.string(),
+    school: z.string(),
+    cellphone: z.string(),
+    tellphone: z.string(),
+    parentsPhone: z.string(),
+    major: z.string(),
+    grade: z.string(),
+  });
