b8ca2dd694260be942b4b09d2891f53c7f615070
EhsanKINUX <ehsankhwentao@gmail.com>
2024-05-29 15:40:17 +0330

    fix: create dashboard header component

---
+++ src/components/pages/dashboard/dashboardLayout/parts/Header/DHeader.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UserAccoutn from "./parts/UserAccoutn";
+
+export default function DHeader() {
+  return (
+    <div className="p-8 w-full border-b-2 border-slate-300 h-10 flex justify-between items-center">
+      <div className="text-black">سلام احسان</div>
+      <UserAccoutn />
+      {/* <div className="text-black bg-slate-400 rounded-xl h-5 w-16"></div> */}
+    </div>
+  );
+}
