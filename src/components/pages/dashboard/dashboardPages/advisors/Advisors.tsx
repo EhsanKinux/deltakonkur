c4baca7bdf43a3b98bdad91cfcc459e096fc8832
EhsanKINUX <ehsankhwentao@gmail.com>
2024-05-31 01:08:39 +0330

    fix: initialize the advisor base ui page

--- src/components/pages/dashboard/dashboardPages/advisors/Advisors.tsx
+++ src/components/pages/dashboard/dashboardPages/advisors/Advisors.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Advisors = () => {
   return (
-    <div>Advisors</div>
-  )
-}
+    <section className="mt-8">
+      <h1 className="border-b-2 border-slate-300 w-fit font-bold text-xl">مشاوران</h1>
+      <div className="flex justify-center items-center w-full gap-3 py-16 mt-4 shadow-sidebar bg-slate-100 rounded-xl"></div>
+    </section>
+  );
+};
 
-export default Advisors
+export default Advisors;
