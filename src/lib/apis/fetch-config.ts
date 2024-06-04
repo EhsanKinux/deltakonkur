bd9a26e48404b163f43621c033357f892b2f82f2
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-04 17:50:00 +0330

    fix: cross origin error bug

--- src/lib/apis/fetch-config.ts
+++ src/lib/apis/fetch-config.ts
@@ -3,7 +3,11 @@ import { BASE_API_URL } from "../variables/variables";
 export async function fetchInstance(url: string, options?: RequestInit) {
   const response = await fetch(`${BASE_API_URL}${url}`, {
     ...options,
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST,PATCH,OPTIONS",
+    },
   });
 
   if (!response.ok) {
--- src/lib/apis/reserve/service.ts
+++ src/lib/apis/reserve/service.ts
@@ -2,4 +2,4 @@ import { fetchInstance } from "../fetch-config";
 import { ISubmitStudentRegisterService } from "./interface";
 
 export const submit_student_register_service = ({ ...body }: ISubmitStudentRegisterService) =>
-  fetchInstance(`api/register/students`, { method: "POST", body: JSON.stringify(body) });
+  fetchInstance(`api/register/students`, { method: "POST", body: JSON.stringify(body), mode: "no-cors" });
