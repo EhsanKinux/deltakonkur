33ff21f0362d59e3ab4a1d626a41ac77273d67d0
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-04 13:08:30 +0330

    feat: create student registeration service interface

---
+++ src/lib/apis/reserve/interface.ts
@@ -0,0 +1,10 @@
+export interface ISubmitStudentRegisterService {
+  first_name: string;
+  last_name: string;
+  phone_number: string;
+  parent_phone: string;
+  home_phone: string;
+  school: string;
+  field: string;
+  grade: number;
+}
