671c82a48b7ac471b4cbbcb4faab9f168c23a666
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-04 17:48:03 +0330

    feat: create sudents separate data table main page

---
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/student/StudentsList.tsx
@@ -0,0 +1,19 @@
+import { appStore } from "@/lib/store/appStore";
+import { DataTable } from "../table/DataTable";
+import { stColumns } from "./table/ColumnStDef";
+
+
+
+const StudentsList = () => {
+    const formData = appStore((state) => state.formData);
+    return (
+      <section className="mt-8">
+        <h1 className="border-b-2 border-slate-300 w-fit font-bold text-xl">دانش‌آموزان</h1>
+        <div className="flex justify-center items-center w-full gap-3 py-16 mt-4 shadow-sidebar bg-slate-100 rounded-xl">
+          <DataTable columns={stColumns} data={formData} />
+        </div>
+      </section>
+    );
+}
+
+export default StudentsList
