b3c5729cc3d34cca5c97331f195ada19429a5ebf
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-04 13:09:21 +0330

    feat: create student submit form api fetch

---
+++ src/lib/apis/reserve/service.ts
@@ -0,0 +1,5 @@
+import { fetchInstance } from "../fetch-config";
+import { ISubmitStudentRegisterService } from "./interface";
+
+export const submit_student_register_service = ({ ...body }: ISubmitStudentRegisterService) =>
+  fetchInstance(`api/register/students`, { method: "POST", body: JSON.stringify(body) });
