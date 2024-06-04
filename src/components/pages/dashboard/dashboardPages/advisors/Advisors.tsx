df1ab7c3108bfd4e2a76f5795a039afc05b11f10
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-04 17:45:55 +0330

    feat: add advisors and student separate card

--- src/components/pages/dashboard/dashboardPages/advisors/Advisors.tsx
+++ src/components/pages/dashboard/dashboardPages/advisors/Advisors.tsx
@@ -1,16 +1,53 @@
-import React from "react";
-import { DataTable } from "./parts/table/DataTable";
-import { columns,  } from "./parts/table/parts/ColumnDef";
-import { appStore } from "@/lib/store/appStore";
+import { useState } from "react";
+import AdvisorList from "./parts/advisor/AdvisorList";
+import StudentsList from "./parts/student/StudentsList";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 const Advisors = () => {
-  const formData = appStore((state) => state.formData);
+  const [view, setView] = useState("cards"); // 'cards', 'advisors', 'students'
+
   return (
     <section className="mt-8">
-      <h1 className="border-b-2 border-slate-300 w-fit font-bold text-xl">مشاوران</h1>
-      <div className="flex justify-center items-center w-full gap-3 py-16 mt-4 shadow-sidebar bg-slate-100 rounded-xl">
-        <DataTable columns={columns} data={formData} />
+      <div className="flex justify-between items-center mb-4">
+        {view == "cards" && (
+          <h1 className="border-b-2 border-slate-300 w-fit font-bold text-xl mx-auto">لیست دسترسی ها</h1>
+        )}
+        {view !== "cards" && (
+          <Button
+            className="pt-4 pb-3 font-bold text-slate-600 border border-slate-500 rounded hover:text-blue-600"
+            onClick={() => setView("cards")}
+          >
+            بازگشت
+          </Button>
+        )}
       </div>
+      {view === "cards" && (
+        <div className="flex justify-center items-center w-full gap-4">
+          <Card
+            className="flex justify-center items-center w-1/3 gap-3 py-16 mt-4 shadow-sidebar bg-slate-100 rounded-xl cursor-pointer"
+            onClick={() => setView("advisors")}
+          >
+            <CardTitle className="text-lg font-semibold">لیست مشاوران</CardTitle>
+          </Card>
+          <Card
+            className="flex flex-col justify-center items-center w-1/3 gap-3 py-16 mt-4 shadow-sidebar bg-slate-100 rounded-xl cursor-pointer"
+            onClick={() => setView("students")}
+          >
+            <CardTitle className="text-lg font-semibold">لیست دانش‌آموزان</CardTitle>
+          </Card>
+        </div>
+      )}
+      {view === "advisors" && (
+        <div>
+          <AdvisorList />
+        </div>
+      )}
+      {view === "students" && (
+        <div>
+          <StudentsList />
+        </div>
+      )}
     </section>
   );
 };
