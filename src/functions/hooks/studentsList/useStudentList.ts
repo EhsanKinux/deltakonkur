b28386666961dabc92c6265a70b92b7a8532841a
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-06 00:16:32 +0330

    fix: bug and loading fix

--- src/functions/hooks/studentsList/useStudentList.ts
+++ src/functions/hooks/studentsList/useStudentList.ts
@@ -1,15 +1,21 @@
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { appStore } from "@/lib/store/appStore";
 import { get_registered_students, students_delete } from "@/lib/apis/reserve/service";
+import { FormEntry } from "@/components/pages/dashboard/dashboardPages/advisors/parts/student/table/interfaces";
 
 export const useStudentList = () => {
   const addFormData = appStore((state) => state.addFormData);
   const deleteFormData = appStore((state) => state.deleteFormData);
+  const [dataLoaded, setDataLoaded] = useState(false);
 
   const getData = useCallback(async () => {
-    const data = await get_registered_students();
-    data.forEach((student: any) => addFormData(student));
-  }, [addFormData]);
+    if (!dataLoaded) {
+      const data = await get_registered_students();
+      data.forEach((student: FormEntry) => addFormData(student));
+      // console.log(data);
+      setDataLoaded(true);
+    }
+  }, [addFormData, dataLoaded]);
 
   const deleteStudent = async (studentId: string) => {
     try {
