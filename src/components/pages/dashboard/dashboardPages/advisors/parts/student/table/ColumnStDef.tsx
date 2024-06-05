69c107bb5b23e939d710188041d14c92d853a178
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-05 11:22:37 +0330

    fix: refactor column

--- src/components/pages/dashboard/dashboardPages/advisors/parts/student/table/ColumnStDef.tsx
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/student/table/ColumnStDef.tsx
@@ -1,26 +1,6 @@
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { ColumnDef } from "@tanstack/react-table";
-import moreIcon from "@/assets/icons/more.svg";
-
-type FormEntry = {
-  id: string;
-  name: string;
-  lastName: string;
-  school: string;
-  cellphone: string;
-  tellphone: string;
-  parentsPhone: string;
-  major: string;
-  grade: string;
-};
+import ColumnDropdown from "./parts/ColumnDropdown";
+import { FormEntry } from "./interfaces";
 
 export const stColumns: ColumnDef<FormEntry>[] = [
   {
@@ -59,24 +39,7 @@ export const stColumns: ColumnDef<FormEntry>[] = [
     id: "actions",
     cell: ({ row }) => {
       const formData = row.original;
-
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <img src={moreIcon} alt="more" className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end" className="bg-slate-200">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem onClick={() => navigator.clipboard.writeText(formData.id)}>حذف</DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>ویرایش</DropdownMenuItem>
-            {/* <DropdownMenuItem></DropdownMenuItem> */}
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
+      return <ColumnDropdown formData={formData} />;
     },
   },
 ];
