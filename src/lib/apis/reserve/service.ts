b0f1a5db242c0cfba67d2d8bea1c3e062168cd50
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-06 00:19:03 +0330

    fix: bug fix

--- src/lib/apis/reserve/service.ts
+++ src/lib/apis/reserve/service.ts
@@ -2,9 +2,9 @@ import { fetchInstance } from "../fetch-config";
 import { ISubmitStudentRegisterService } from "./interface";
 
 export const submit_student_register_service = ({ ...body }: ISubmitStudentRegisterService) =>
-  fetchInstance(`api/register/students`, { method: "POST", body: JSON.stringify(body) });
+  fetchInstance(`api/register/students/`, { method: "POST", body: JSON.stringify(body) });
 
 export const get_registered_students = () => fetchInstance(`api/register/students`);
 
-export const students_delete = ({ studentId, ...body }: { studentId: string }) =>
-  fetchInstance(`api/register/students/${studentId}`, { method: "DELETE", body: JSON.stringify(body) });
+export const students_delete = ({ studentId }: { studentId: string }) =>
+  fetchInstance(`api/register/students/${studentId}/`, { method: "DELETE" });
