0896d21522c81125dc337e75fb0412a6fa6e213f
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-05 11:24:56 +0330

    fix: refactore fetch functionality

--- src/lib/apis/fetch-config.ts
+++ src/lib/apis/fetch-config.ts
@@ -1,18 +1,18 @@
 import { BASE_API_URL } from "../variables/variables";
 
 export async function fetchInstance(url: string, options?: RequestInit) {
-  const response = await fetch(`${BASE_API_URL}${url}`, {
+  return await fetch(`${BASE_API_URL}${url}`, {
     ...options,
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "POST,PATCH,OPTIONS",
     },
+  }).then(async (response) => {
+    if (!response.ok) {
+      const message = await response.text();
+      throw new Error(message || "Network Error!");
+    }
+    return response.json();
   });
-
-  if (!response.ok) {
-    const message = await response.text();
-    throw new Error(message || "Network Error!");
-  }
-  return response.json();
 }
