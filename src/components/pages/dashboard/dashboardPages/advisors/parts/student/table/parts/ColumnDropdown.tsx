ddf51e4b01e061b329de77e8c2733ac83b3d71e8
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-05 11:23:48 +0330

    feat: separate the dropdown for each row and add delete student functionality

---
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/student/table/parts/ColumnDropdown.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useStudentList } from "@/functions/hooks/studentsList/useStudentList";
+import moreIcon from "@/assets/icons/more.svg";
+import { FormEntry } from "../interfaces";
+
+const ColumnDropdown = ({ formData }: { formData: FormEntry }) => {
+  const { deleteStudent } = useStudentList();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-8 p-0">
+          <span className="sr-only">Open menu</span>
+          <img src={moreIcon} alt="more" className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="bg-slate-200">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuItem onClick={() => deleteStudent(formData.id)}>حذف</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>ویرایش</DropdownMenuItem>
+        {/* <DropdownMenuItem></DropdownMenuItem> */}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default ColumnDropdown;
