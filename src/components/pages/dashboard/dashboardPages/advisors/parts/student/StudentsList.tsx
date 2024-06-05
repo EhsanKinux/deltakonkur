e34da9d2018988b2a8ec48dc8ead529281802294
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-06 00:11:58 +0330

    fix: ui style fix

--- src/components/pages/dashboard/dashboardPages/advisors/parts/student/StudentsList.tsx
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/student/StudentsList.tsx
@@ -2,7 +2,8 @@ import { appStore } from "@/lib/store/appStore";
 import { DataTable } from "../table/DataTable";
 import { stColumns } from "./table/ColumnStDef";
 import { useStudentList } from "@/functions/hooks/studentsList/useStudentList";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const StudentsList = () => {
   const formData = appStore((state) => state.formData);
@@ -12,12 +13,16 @@ const StudentsList = () => {
     getData();
   }, [getData]);
 
+  const memoizedFormData = useMemo(() => formData, [formData]);
+
   return (
     <section className="mt-8">
       <h1 className="border-b-2 border-slate-300 w-fit font-bold text-xl">دانش‌آموزان</h1>
-      <div className="flex justify-center items-center w-full gap-3 py-16 mt-4 shadow-sidebar bg-slate-100 rounded-xl">
-        <DataTable columns={stColumns} data={formData} />
-      </div>
+      <ScrollArea className="min-h-screen w-full rounded-md border p-4">
+        <div className="flex justify-center items-center w-full gap-3 py-16 mt-4 shadow-sidebar bg-slate-100 rounded-xl">
+          <DataTable columns={stColumns} data={memoizedFormData} />
+        </div>
+      </ScrollArea>
     </section>
   );
 };
