9ff2888318db0af1475c4e61bb25ebc51f4a2344
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-05 11:23:04 +0330

    fix: add the interface

---
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/student/table/interfaces.ts
@@ -0,0 +1,11 @@
+export type FormEntry = {
+  id: string;
+  name: string;
+  lastName: string;
+  school: string;
+  cellphone: string;
+  tellphone: string;
+  parentsPhone: string;
+  major: string;
+  grade: string;
+};
