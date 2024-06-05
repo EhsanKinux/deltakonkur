ae4964779a685083deb8dbd23d5f6596e8aad322
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-05 11:22:09 +0330

    fix: fetching students data

--- src/components/pages/dashboard/dashboardPages/advisors/parts/student/StudentsList.tsx
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/student/StudentsList.tsx
@@ -1,19 +1,25 @@
 import { appStore } from "@/lib/store/appStore";
 import { DataTable } from "../table/DataTable";
 import { stColumns } from "./table/ColumnStDef";
-
-
+import { useStudentList } from "@/functions/hooks/studentsList/useStudentList";
+import { useEffect } from "react";
 
 const StudentsList = () => {
-    const formData = appStore((state) => state.formData);
-    return (
-      <section className="mt-8">
-        <h1 className="border-b-2 border-slate-300 w-fit font-bold text-xl">دانش‌آموزان</h1>
-        <div className="flex justify-center items-center w-full gap-3 py-16 mt-4 shadow-sidebar bg-slate-100 rounded-xl">
-          <DataTable columns={stColumns} data={formData} />
-        </div>
-      </section>
-    );
-}
+  const formData = appStore((state) => state.formData);
+  const { getData } = useStudentList();
 
-export default StudentsList
+  useEffect(() => {
+    getData();
+  }, [getData]);
+
+  return (
+    <section className="mt-8">
+      <h1 className="border-b-2 border-slate-300 w-fit font-bold text-xl">دانش‌آموزان</h1>
+      <div className="flex justify-center items-center w-full gap-3 py-16 mt-4 shadow-sidebar bg-slate-100 rounded-xl">
+        <DataTable columns={stColumns} data={formData} />
+      </div>
+    </section>
+  );
+};
+
+export default StudentsList;
