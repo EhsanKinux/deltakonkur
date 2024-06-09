0dbf85c82f5f091f2be0aa0f5806375ae86ea25e
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-09 22:34:05 +0330

    fix: delete dialog close button triggering bug problem

--- src/components/pages/dashboard/dashboardPages/advisors/parts/student/table/parts/edit/DeleteConfirmationDialog.tsx
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/student/table/parts/edit/DeleteConfirmationDialog.tsx
@@ -1,5 +1,12 @@
 import { Button } from "@/components/ui/button";
-import { DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import { useStudentList } from "@/functions/hooks/studentsList/useStudentList";
 import { FormEntry } from "../../interfaces";
 
@@ -17,9 +24,6 @@ const DeleteConfirmationDialog = ({
     setDeleteDialogOpen(false);
   };
 
-  const handleDeleteCancel = () => {
-    // setDeleteDialogOpen(false);
-  };
   return (
     <DialogContent className="bg-slate-100 !rounded-[10px]">
       <DialogHeader>
@@ -31,12 +35,9 @@ const DeleteConfirmationDialog = ({
           <Button className="bg-blue-500 text-white hover:bg-blue-700 rounded-xl pt-2" onClick={handleDeleteConfirm}>
             حذف دانش‌آموز
           </Button>
-          <Button
-            className="bg-gray-300 text-black hover:bg-slate-700 hover:text-white rounded-xl pt-2"
-            onClick={handleDeleteCancel}
-          >
-            لغو
-          </Button>
+          <DialogClose asChild>
+            <Button className="bg-gray-300 text-black hover:bg-slate-700 hover:text-white rounded-xl pt-2">لغو</Button>
+          </DialogClose>
         </div>
       </DialogFooter>
     </DialogContent>
