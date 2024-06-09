6cb2f2a6a8b646d1432192cd5088f4ae62083d02
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-09 17:10:44 +0330

    fix: add deleting dialog and fix the dropdown menue dialog

--- src/components/pages/dashboard/dashboardPages/advisors/parts/student/table/parts/ColumnDropdown.tsx
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/student/table/parts/ColumnDropdown.tsx
@@ -7,37 +7,50 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { useStudentList } from "@/functions/hooks/studentsList/useStudentList";
+// import { useStudentList } from "@/functions/hooks/studentsList/useStudentList";
 import moreIcon from "@/assets/icons/more.svg";
 import { FormEntry } from "../interfaces";
 import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { EditStudentDialog } from "./edit/EditStudentDialog";
+import { useState } from "react";
+import DeleteConfirmationDialog from "./edit/DeleteConfirmationDialog";
 
 const ColumnDropdown = ({ formData }: { formData: FormEntry }) => {
-  const { deleteStudent } = useStudentList();
+  // const { deleteStudent } = useStudentList();
+  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
+  const [editDialogOpen, setEditDialogOpen] = useState(false);
+
   console.log(formData);
+
   return (
-    <Dialog>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" className="h-8 w-8 p-0">
-            <span className="sr-only">Open menu</span>
-            <img src={moreIcon} alt="more" className="h-4 w-4" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end" className="bg-slate-200 rounded-xl">
-          <DropdownMenuLabel>اقدامات</DropdownMenuLabel>
-          <DropdownMenuItem className="cursor-pointer" onClick={() => deleteStudent(formData?.id)}>
-            حذف
-          </DropdownMenuItem>
-          <DropdownMenuSeparator className="bg-slate-300" />
-          <DialogTrigger>
-            <DropdownMenuItem className="cursor-pointer">ویرایش</DropdownMenuItem>
-          </DialogTrigger>
-        </DropdownMenuContent>
-      </DropdownMenu>
-      <EditStudentDialog />
-    </Dialog>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-8 p-0">
+          <span className="sr-only">Open menu</span>
+          <img src={moreIcon} alt="more" className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="bg-slate-200 rounded-xl">
+        <DropdownMenuLabel>اقدامات</DropdownMenuLabel>
+        <DropdownMenuItem className="cursor-pointer" onClick={() => setDeleteDialogOpen(true)}>
+          حذف
+        </DropdownMenuItem>
+        <DropdownMenuSeparator className="bg-slate-300" />
+        <DropdownMenuItem className="cursor-pointer" onClick={() => setEditDialogOpen(true)}>
+          ویرایش
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+      <Dialog open={editDialogOpen} onOpenChange={setEditDialogOpen}>
+        <DialogTrigger>
+          <EditStudentDialog />
+        </DialogTrigger>
+      </Dialog>
+      <Dialog open={deleteDialogOpen} onOpenChange={setDeleteDialogOpen}>
+        <DialogTrigger>
+          <DeleteConfirmationDialog setDeleteDialogOpen={setDeleteDialogOpen} formData={formData} />
+        </DialogTrigger>
+      </Dialog>
+    </DropdownMenu>
   );
 };
 
