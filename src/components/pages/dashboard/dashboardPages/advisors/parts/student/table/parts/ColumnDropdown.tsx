72417e567a7c08614e6ec81f143e5c1769f68ced
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-09 16:21:04 +0330

    feat: add editing dialog to the dropdown

--- src/components/pages/dashboard/dashboardPages/advisors/parts/student/table/parts/ColumnDropdown.tsx
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/student/table/parts/ColumnDropdown.tsx
@@ -10,26 +10,34 @@ import {
 import { useStudentList } from "@/functions/hooks/studentsList/useStudentList";
 import moreIcon from "@/assets/icons/more.svg";
 import { FormEntry } from "../interfaces";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { EditStudentDialog } from "./edit/EditStudentDialog";
 
 const ColumnDropdown = ({ formData }: { formData: FormEntry }) => {
   const { deleteStudent } = useStudentList();
   console.log(formData);
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="h-8 w-8 p-0">
-          <span className="sr-only">Open menu</span>
-          <img src={moreIcon} alt="more" className="h-4 w-4" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="bg-slate-200">
-        <DropdownMenuLabel>اقدامات</DropdownMenuLabel>
-        <DropdownMenuItem className="cursor-pointer" onClick={() => deleteStudent(formData?.id)}>حذف</DropdownMenuItem>
-        <DropdownMenuSeparator className="bg-slate-300" />
-        <DropdownMenuItem className="cursor-pointer" >ویرایش</DropdownMenuItem>
-        {/* <DropdownMenuItem></DropdownMenuItem> */}
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <img src={moreIcon} alt="more" className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="bg-slate-200 rounded-xl">
+          <DropdownMenuLabel>اقدامات</DropdownMenuLabel>
+          <DropdownMenuItem className="cursor-pointer" onClick={() => deleteStudent(formData?.id)}>
+            حذف
+          </DropdownMenuItem>
+          <DropdownMenuSeparator className="bg-slate-300" />
+          <DialogTrigger>
+            <DropdownMenuItem className="cursor-pointer">ویرایش</DropdownMenuItem>
+          </DialogTrigger>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <EditStudentDialog />
+    </Dialog>
   );
 };
 
