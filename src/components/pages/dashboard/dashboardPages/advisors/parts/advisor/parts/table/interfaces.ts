abe0381b99b975f38824f4e2fb1b5859e6a56241
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-16 01:51:48 +0330

    fix: type fix

--- src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/parts/table/interfaces.ts
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/parts/table/interfaces.ts
@@ -1,13 +1,9 @@
 export type FormEntry = {
   id: string;
-  name: string;
-  lastName: string;
-  id_number: string;
-  num_of_students: string;
-  active_st: string;
-  canceled_st: string;
-  stoped_st: string;
-  saticfaction: string;
-  advisor_level: string;
-  account_num: string;
+  first_name: string;
+  last_name: string;
+  field: string;
+  phone_number: string;
+  national_id: string;
+  bank_account: string;
 };
