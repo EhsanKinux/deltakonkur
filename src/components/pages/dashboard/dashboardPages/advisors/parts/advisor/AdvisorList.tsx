9dd3f284cada95bd9ebcf7615e51f1b036f1a870
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-15 15:00:41 +0330

    fix: craete two tabs for advisors table and advisors registeration

--- src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/AdvisorList.tsx
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/AdvisorList.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { DataTable } from "../table/DataTable";
 import { columns } from "./parts/table/ColumnDef";
+import backIcon from "@/assets/icons/back.svg";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const AdvisorList = ({ setView }: { setView: React.Dispatch<React.SetStateAction<string>> }) => {
   const FormEntry = [
@@ -23,14 +25,28 @@ const AdvisorList = ({ setView }: { setView: React.Dispatch<React.SetStateAction
       <div className="flex justify-between">
         <h1 className="border-b-2 border-slate-300 w-fit font-bold text-xl">مشاوران</h1>
         <Button
-          className="pt-4 pb-3 font-bold text-slate-600 border border-slate-500 rounded hover:text-blue-600"
+          className="flex gap-2 pt-4 pb-3 font-bold text-base text-slate-600 rounded hover:text-blue-600"
           onClick={() => setView("cards")}
         >
-          بازگشت
+          <img className="w-5 pb-[2px]" src={backIcon} alt="backIcon" />
+          <span>بازگشت</span>
         </Button>
       </div>
       <div className="flex justify-center items-center w-full gap-3 py-16 mt-4 shadow-sidebar bg-slate-100 rounded-xl">
-        <DataTable columns={columns} data={FormEntry} />
+        <Tabs defaultValue="advisorsTable" className="flex flex-col gap-4 justify-center items-center">
+          <TabsList className="bg-slate-50 rounded-xl shadow-form flex gap-4 justify-center items-center">
+            <TabsTrigger className="rounded-xl data-[state=active]:bg-slate-200" value="advisorsTable">
+              لیست مشاوران
+            </TabsTrigger>
+            <TabsTrigger className="rounded-xl data-[state=active]:bg-slate-200" value="register">
+              ثبت مشاور جدید
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="advisorsTable">
+            <DataTable columns={columns} data={FormEntry} />
+          </TabsContent>
+          <TabsContent value="register">Make changes to your account here.</TabsContent>
+        </Tabs>
       </div>
     </section>
   );
