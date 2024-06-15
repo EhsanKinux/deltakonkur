8efe5e564a08b6d05bb8858ddd239d4edd472d32
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-16 02:01:06 +0330

    fix: style fix

--- src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/AdvisorList.tsx
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/AdvisorList.tsx
@@ -31,11 +31,11 @@ const AdvisorList = ({ setView }: { setView: React.Dispatch<React.SetStateAction
       </div>
       <div className="flex justify-center items-center w-full gap-3 py-16 mt-4 shadow-sidebar bg-slate-100 rounded-xl">
         <Tabs defaultValue="advisorsTable" className="flex flex-col gap-4 justify-center items-center">
-          <TabsList className="bg-slate-50 rounded-xl shadow-form flex gap-4 justify-center items-center">
-            <TabsTrigger className="rounded-xl data-[state=active]:bg-slate-200" value="advisorsTable">
+          <TabsList className="bg-slate-200 rounded-xl shadow-form flex gap-4 justify-center items-center">
+            <TabsTrigger className="rounded-xl data-[state=active]:bg-blue-200" value="advisorsTable">
               لیست مشاوران
             </TabsTrigger>
-            <TabsTrigger className="rounded-xl data-[state=active]:bg-slate-200" value="register">
+            <TabsTrigger className="rounded-xl data-[state=active]:bg-blue-200" value="register">
               ثبت مشاور جدید
             </TabsTrigger>
           </TabsList>
