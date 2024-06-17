c2beeee693ab1651685edbe1762b28a4d0321584
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-17 09:49:11 +0330

    fix: remove component

--- src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/parts/table/ColumnDef.tsx
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/parts/table/ColumnDef.tsx
@@ -1,15 +1,6 @@
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { ColumnDef } from "@tanstack/react-table";
-import moreIcon from "@/assets/icons/more.svg";
 import { FormEntry } from "./interfaces";
+import ColumnDropdown from "./parts/ColumnDropdown";
 
 export const columns: ColumnDef<FormEntry>[] = [
   {
@@ -41,23 +32,7 @@ export const columns: ColumnDef<FormEntry>[] = [
     cell: ({ row }) => {
       const formData = row.original;
 
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <img src={moreIcon} alt="more" className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end" className="bg-slate-200">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem onClick={() => navigator.clipboard.writeText(formData.id)}>حذف</DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>ویرایش</DropdownMenuItem>
-            {/* <DropdownMenuItem></DropdownMenuItem> */}
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
+      return <ColumnDropdown formData={formData} />;
     },
   },
 ];
