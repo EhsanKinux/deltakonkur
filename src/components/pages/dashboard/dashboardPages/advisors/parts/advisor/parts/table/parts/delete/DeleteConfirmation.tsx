4d19f069dd6b3d4f6348931368c2cd39f5162917
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-17 09:50:16 +0330

    feat: create delete dialog

---
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/parts/table/parts/delete/DeleteConfirmation.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@/components/ui/button";
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useStudentList } from "@/functions/hooks/studentsList/useStudentList";
+import { FormEntry } from "../../interfaces";
+
+const DeleteConfirmation = ({
+  setDeleteDialogOpen,
+  formData,
+}: {
+  setDeleteDialogOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  formData: FormEntry;
+}) => {
+//   const { deleteStudent } = useStudentList();
+
+  const handleDeleteConfirm = () => {
+    // deleteStudent(formData?.id);
+    setDeleteDialogOpen(false);
+  };
+
+  return (
+    <DialogContent className="bg-slate-100 !rounded-[10px]">
+      <DialogHeader>
+        <DialogTitle>تایید حذف</DialogTitle>
+        <DialogDescription>آیا از حذف این مشاور مطمئن هستید؟</DialogDescription>
+      </DialogHeader>
+      <DialogFooter>
+        <div className="flex justify-between items-center w-full">
+          <Button className="bg-blue-500 text-white hover:bg-blue-700 rounded-xl pt-2" onClick={handleDeleteConfirm}>
+            حذف مشاور
+          </Button>
+          <DialogClose asChild>
+            <Button className="bg-gray-300 text-black hover:bg-slate-700 hover:text-white rounded-xl pt-2">لغو</Button>
+          </DialogClose>
+        </div>
+      </DialogFooter>
+    </DialogContent>
+  );
+};
+
+export default DeleteConfirmation;
