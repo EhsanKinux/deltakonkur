b7a9c93400ec31d7ce156c3e0ae74c105d444dbf
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-27 09:24:57 +0330

    feat: create advisor delete api call

--- src/lib/apis/advisors/service.ts
+++ src/lib/apis/advisors/service.ts
@@ -6,3 +6,6 @@ export const get_registered_advisors = () => fetchInstance(`api/advisor/advisors
 export const submit_advisors_register_service = ({ ...body }: ISubmitAdvisorRegisterService) => {
   fetchInstance(`api/advisor/advisors/`, { method: "POST", body: JSON.stringify(body) });
 };
+
+export const advisors_delete = ({ advisorId }: { advisorId: string }) =>
+  fetchInstance(`api/advisor/advisors/${advisorId}/`, { method: "DELETE" });
