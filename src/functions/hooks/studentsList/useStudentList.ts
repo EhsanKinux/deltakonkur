439801e082e919c8aff720cbf68c1b10bff33be7
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-28 22:48:23 +0330

    feat: create set advisor for student hook function

--- src/functions/hooks/studentsList/useStudentList.ts
+++ src/functions/hooks/studentsList/useStudentList.ts
@@ -6,9 +6,11 @@ import { ISubmitStudentRegisterService } from "@/lib/apis/reserve/interface";
 import {
   get_registered_students,
   get_student_info,
+  set_student_advisor,
   students_delete,
   update_student_info,
 } from "@/lib/apis/students/service";
+import { ISetStudentAdvisor } from "@/lib/apis/students/interface";
 
 export const useStudentList = () => {
   const addFormData = appStore((state) => state.addFormData);
@@ -88,6 +90,36 @@ export const useStudentList = () => {
     [setError, setLoading, setStudentInfo]
   );
 
+  const setAdvisorForStudent = useCallback(
+    async ({ studentId, advisorId }: { studentId: string; advisorId: string }) => {
+      setLoading(true);
+      setError(null);
+      try {
+        const body: ISetStudentAdvisor = {
+          student: studentId,
+          advisor: advisorId,
+          status: "active",
+        };
+        const response = await set_student_advisor({ body });
+        if (response.ok) {
+          const updatedInfo = await response.json();
+          console.table("SetAdvisor:", updatedInfo);
+        } else {
+          setError("Failed to set advisor for student");
+        }
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError("Failed to set advisor for student");
+        }
+      } finally {
+        setLoading(false);
+      }
+    },
+    [setError, setLoading]
+  );
+
   return {
     deleteStudent,
     getData,
@@ -96,5 +128,6 @@ export const useStudentList = () => {
     error,
     fetchStudentInfo,
     updateStudentInfo,
+    setAdvisorForStudent,
   };
 };
