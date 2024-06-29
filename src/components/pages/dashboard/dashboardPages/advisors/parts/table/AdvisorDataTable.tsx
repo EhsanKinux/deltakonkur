e43d0eef7d20bd4bfb3132c00a94e9a65515892b
EhsanKINUX <ehsankhwentao@gmail.com>
2024-06-29 12:26:41 +0330

    feat: create new data table for advisor and make the rows clickable

---
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/table/AdvisorDataTable.tsx
@@ -0,0 +1,96 @@
+import { ColumnDef, flexRender, getCoreRowModel, useReactTable, getPaginationRowModel } from "@tanstack/react-table";
+
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
+import { Advisor } from "@/lib/store/types";
+
+interface AdvisorDataTableProps{
+  columns: ColumnDef<Advisor>[];
+  data: Advisor[];
+}
+
+export function AdvisorDataTable({ columns, data }: AdvisorDataTableProps) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    initialState: {
+      pagination: {
+        pageSize: 8,
+      },
+    },
+  });
+
+  const navigate = useNavigate();
+
+  const handleRowClick = (advisorId: string) => {
+    navigate(`/dashboard/advisors/${advisorId}`);
+  };
+
+  return (
+    <div>
+      <div className="!rounded-xl border overflow-hidden">
+        <Table>
+          <TableHeader className="bg-slate-300">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row, index) => (
+                <TableRow
+                  className="hover:bg-slate-200 hover:cursor-pointer"
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                  onClick={() => handleRowClick(data[index].id)}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24 text-center">
+                  نتیجه ای یافت نشد...
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-between space-x-2 py-4">
+        <div className="flex gap-2">
+          <Button
+            className="rounded-[8px] border border-black"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            قبلی
+          </Button>
+          <Button
+            className="rounded-[8px] border border-black"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            بعدی
+          </Button>
+        </div>
+        <span className="text-slate-600 text-sm">
+          صفحه {table.getState().pagination.pageIndex + 1} از {table.getPageCount()}
+        </span>
+      </div>
+    </div>
+  );
+}
