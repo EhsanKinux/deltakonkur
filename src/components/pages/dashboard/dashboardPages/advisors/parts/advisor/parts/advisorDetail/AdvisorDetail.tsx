8365f8ab826101eba2764eecb3584c4af01fec86
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-01 17:51:30 +0330

    fix: mentioning new data

--- src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/parts/advisorDetail/AdvisorDetail.tsx
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/parts/advisorDetail/AdvisorDetail.tsx
@@ -20,13 +20,17 @@ const AdvisorDetail = () => {
   }
 
   return (
-    <div>
-      <h1>Advisor Detail</h1>
-      <p>ID: {advisorInfo?.id}</p>
-      <p>نام: {advisorInfo?.first_name}</p>
-      <p>نام خانوادگی: {advisorInfo?.last_name}</p>
-      <p>شماره تماس: {advisorInfo?.phone_number}</p>
-      <p>رشته ی تحصیلی: {advisorInfo?.field}</p>
+    <div className="flex justify-center items-center w-full gap-3 py-16 mt-4 shadow-sidebar bg-slate-100 rounded-xl relative">
+      <h1 className="absolute top-4 right-4 text-3xl font-bold text-gray-800">
+        {advisorInfo?.first_name} {advisorInfo?.last_name}
+      </h1>
+      <div className="flex items-center gap-3 p-4">
+        <span className="text-lg font-medium"> شماره تماس مشاور: {advisorInfo?.phone_number}</span>
+        <span className="text-lg font-medium">رشته ی تحصیلی: {advisorInfo?.field}</span>
+        <span>دانش آموزان فعال: {advisorInfo?.active_students}</span>
+        <span>دانش آموزان کنسلی: {advisorInfo?.cancelled_students}</span>
+        <span>دانش آموزان متوقف شده: {advisorInfo?.stopped_students}</span>
+      </div>
     </div>
   );
 };
