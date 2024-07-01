7b43c93ac9b20060ae71553cf7a227a46aab41ed
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-01 17:43:12 +0330

    fix: form handling and control added

--- src/components/pages/dashboard/dashboardPages/advisors/parts/student/table/parts/edit/parts/selectAdvisor/SelectStudentAdvisor.tsx
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/student/table/parts/edit/parts/selectAdvisor/SelectStudentAdvisor.tsx
@@ -1,7 +1,7 @@
 import { UseFormReturn } from "react-hook-form";
 import CustomAdvisorSelect from "./CustomAdvisorSelect";
 import { Advisor } from "@/lib/store/types";
-import { FormDescription, FormLabel, FormMessage } from "@/components/ui/form";
+import { FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 
 const SelectStudentAdvisor = ({
   form,
@@ -29,23 +29,27 @@ const SelectStudentAdvisor = ({
   const advisorValue = watch("advisor");
 
   return (
-    <div className="flex flex-col !gap-2 bg-slate-100 rounded-xl">
-      <FormLabel className="pr-5">تعیین مشاور</FormLabel>
-      <CustomAdvisorSelect
-        {...register("advisor")}
-        value={advisorValue}
-        onValueChange={(value: string) => setValue("advisor", value)}
-        placeholder="انتخاب مشاور"
-        options={memoizedAdvisors.map((advisor) => ({
-          value: advisor.first_name,
-          label: `${advisor.first_name} ${advisor.last_name}`,
-        }))}
-      />
-      <FormDescription className="pr-5">
-        مشاور دانش آموز را تعیین کنید!
-      </FormDescription>
-      <FormMessage />
-    </div>
+    <FormField
+      control={form.control}
+      name="advisor"
+      render={() => (
+        <FormItem className="flex flex-col bg-slate-100 rounded-xl">
+          <FormLabel className="pr-5">تعیین مشاور</FormLabel>
+          <CustomAdvisorSelect
+            {...register("advisor")}
+            value={advisorValue}
+            onValueChange={(value: string) => setValue("advisor", value)}
+            placeholder="انتخاب مشاور"
+            options={memoizedAdvisors.map((advisor) => ({
+              value: String(advisor.id),
+              label: `${advisor.first_name} ${advisor.last_name}`,
+            }))}
+          />
+          <FormDescription className="pr-5">مشاور دانش آموز را تعیین کنید!</FormDescription>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
   );
 };
 
