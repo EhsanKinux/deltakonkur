48365b4619467a84687d7958f6180b2813f29072
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-02 16:36:22 +0330

    fix: student update and student advisor update bug

--- src/lib/apis/students/service.ts
+++ src/lib/apis/students/service.ts
@@ -10,13 +10,8 @@ export const students_delete = ({ studentId }: { studentId: string }) =>
 export const get_student_info = ({ studentId }: { studentId: string }) =>
   fetchInstance(`api/register/students/${studentId}/`, { method: "GET" });
 
-export const update_student_info = ({
-  studentId,
-  ...body
-}: {
-  studentId: string;
-  body: ISubmitStudentRegisterService;
-}) => fetchInstance(`api/register/students/${studentId}/`, { method: "PATCH", body: JSON.stringify(body) });
+export const update_student_info = (body: ISubmitStudentRegisterService) =>
+  fetchInstance(`api/register/students/${body.id}/`, { method: "PATCH", body: JSON.stringify(body) });
 
-export const set_student_advisor = ({ ...body }: { body: ISetStudentAdvisor }) =>
+export const set_student_advisor = (body: ISetStudentAdvisor) =>
   fetchInstance(`api/register/student-advisors/`, { method: "POST", body: JSON.stringify(body) });
