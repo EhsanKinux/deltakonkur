fcf4d9ef4767c77464b2590b91f62d2f24e5b904
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-06 00:45:22 +0330

    delete: unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,6 @@ import { Outlet } from "react-router-dom";
 function App() {
   return (
     <div className="flex flex-col justify-center items-center">
-      <nav className="flex flex-col justify-center items-center w-full">
-        {/* <Link to="/auth/signIn" className="mb-2 px-4 py-2 text-white bg-blue-500 rounded">
-          Sign In
-        </Link>
-        <Link to="/auth/signUp" className="mb-2 px-4 py-2 text-white bg-blue-500 rounded">
-          Sign Up
-        </Link>
-        <Link to="/dashboard" className="mb-2 px-4 py-2 text-white bg-blue-500 rounded">
-          Dashboard
-        </Link> */}
-        {/* <AuthLayout /> */}
-      </nav>
       <Outlet />
     </div>
   );
