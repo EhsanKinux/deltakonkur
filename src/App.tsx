d2da33f021b8d56a469fe6eb1ebd73b035ccd64e
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-09 10:43:43 +0330

    feat: add Toaster wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { Toaster } from "sonner";
 
 function App() {
   return (
     <div className="flex flex-col justify-center items-center">
+      <Toaster richColors position="bottom-right" />
       <Outlet />
     </div>
   );
