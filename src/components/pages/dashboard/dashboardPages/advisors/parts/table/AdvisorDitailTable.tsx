b578c3ce87b979b9bdd3fed317cbb6c35f9256ad
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-11 11:53:28 +0330

    feat: create student table for advisor detail page

---
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/table/AdvisorDitailTable.tsx
@@ -0,0 +1,93 @@
+import { ColumnDef, flexRender, getCoreRowModel, useReactTable, getPaginationRowModel } from "@tanstack/react-table";
+
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { ProcessedStudentData } from "../advisor/parts/advisorDetail/interface";
+
+interface AdvisorDitailTableProps {
+  columns: ColumnDef<ProcessedStudentData>[];
+  data: ProcessedStudentData[];
+}
+
+export function AdvisorDitailTable({ columns, data }: AdvisorDitailTableProps) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    initialState: {
+      pagination: {
+        pageSize: 8,
+      },
+    },
+  });
+
+  const getRowBgColor = (status: string | null) => {
+    if (status === "active") return "bg-green-200";
+    if (status === "stop") return "bg-red-200";
+    return "bg-orange-200";
+  };
+
+  return (
+    <div className="w-full overflow-hidden p-10 absolute top-0 right-0 left-0 bottom-0">
+      <Table className="!rounded-xl border">
+        <TableHeader className="bg-slate-300">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                className={`hover:bg-slate-200 ${getRowBgColor(row.original.status)}`}
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                نتیجه ای یافت نشد...
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+
+      <div className="flex items-center justify-between py-4 w-full">
+        <div className="flex gap-2">
+          <Button
+            className="rounded-[8px] border border-black"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            قبلی
+          </Button>
+          <Button
+            className="rounded-[8px] border border-black"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            بعدی
+          </Button>
+        </div>
+        <span className="text-slate-600 text-sm">
+          صفحه {table.getState().pagination.pageIndex + 1} از {table.getPageCount()}
+        </span>
+      </div>
+    </div>
+  );
+}
