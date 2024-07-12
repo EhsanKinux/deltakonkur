2384c407faa25959baf719c2537c8c059e88b442
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-12 11:33:31 +0330

    feat: create column definition for all students table in accounting part

---
+++ src/components/pages/dashboard/dashboardPages/accounting/allStudents/parts/ColumnAccountingStDef.tsx
@@ -0,0 +1,48 @@
+import { ColumnDef } from "@tanstack/react-table";
+import { IAccountingStudents } from "./interfaces";
+
+export const accountingStColumns: ColumnDef<IAccountingStudents>[] = [
+  {
+    accessorKey: "first_name",
+    header: "نام",
+  },
+  {
+    accessorKey: "last_name",
+    header: "نام خانوادگی",
+  },
+  {
+    accessorKey: "school",
+    header: "نام مدرسه",
+  },
+  {
+    accessorKey: "phone_number",
+    header: "شماره همراه",
+  },
+  {
+    accessorKey: "home_phone",
+    header: "شماره تلفن منزل",
+  },
+  {
+    accessorKey: "parent_phone",
+    header: "شماره همراه والدین",
+  },
+  {
+    accessorKey: "field",
+    header: "رشته تحصیلی",
+  },
+  {
+    accessorKey: "grade",
+    header: "مقطع تحصیلی",
+  },
+  {
+    accessorKey: "created",
+    header: "تاریخ ثبت",
+  },
+//   {
+//     id: "actions",
+//     cell: ({ row }) => {
+//       const formData = row.original;
+//       return <StudentDialogButtons formData={formData} />;
+//     },
+//   },
+];
---
+++ src/components/pages/dashboard/dashboardPages/accounting/allStudents/parts/interfaces.ts
@@ -0,0 +1,12 @@
+export type IAccountingStudents = {
+  id: string;
+  first_name: string;
+  last_name: string;
+  school: string;
+  phone_number: string;
+  home_phone: string;
+  parent_phone: string;
+  field: string;
+  grade: string;
+  created: string;
+};
