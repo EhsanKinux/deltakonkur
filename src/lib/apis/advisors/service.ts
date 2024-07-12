c01cc5549845c776e1b202c7ca7a392f5ab88f25
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-12 11:40:55 +0330

    fix: missing define method

--- src/lib/apis/advisors/service.ts
+++ src/lib/apis/advisors/service.ts
@@ -1,7 +1,7 @@
 import { fetchInstance } from "../fetch-config";
 import { ISubmitAdvisorRegisterService } from "./interface";
 
-export const get_registered_advisors = () => fetchInstance(`api/advisor/advisors/`);
+export const get_registered_advisors = () => fetchInstance(`api/advisor/advisors/`, { method: "GET" });
 
 export const submit_advisors_register_service = ({ ...body }: ISubmitAdvisorRegisterService) => {
   return fetchInstance(`api/advisor/advisors/`, { method: "POST", body: JSON.stringify(body) });
