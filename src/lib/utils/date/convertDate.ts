ff0fc73f246b2f1f7bed74b9cba537984ee88385
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-15 19:13:08 +0330

    feat: create 2 date converter, one for convert into Shamsi, other one for convert into Gregorian

--- src/lib/utils/date/convertDate.ts
+++ src/lib/utils/date/convertDate.ts
@@ -12,3 +12,24 @@ export const convertToShamsi = (isoDate: string) => {
 
   return `${shamsiDate.jd} / ${shamsiDate.jm} / ${shamsiDate.jy}`;
 };
+
+export const convertToShamsi2 = (isoDate: string) => {
+  const date = new Date(isoDate);
+  const gregorianDate = {
+    gy: date.getFullYear(),
+    gm: date.getMonth() + 1,
+    gd: date.getDate(),
+  };
+
+  const shamsiDate = jalaali.toJalaali(gregorianDate.gy, gregorianDate.gm, gregorianDate.gd);
+
+  return `${shamsiDate.jy}-${shamsiDate.jm}-${shamsiDate.jd}`;
+};
+
+export const convertToGregorian = (shamsiDate: string) => {
+  const [jy, jm, jd] = shamsiDate.split("-").map(Number);
+  const gregorianDate = jalaali.toGregorian(jy, jm, jd);
+
+  const date = new Date(gregorianDate.gy, gregorianDate.gm - 2, gregorianDate.gd);
+  return date.toISOString().split("T")[0];
+};
