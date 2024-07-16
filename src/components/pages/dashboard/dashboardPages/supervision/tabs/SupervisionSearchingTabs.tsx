0d1ea0cf38bde099e0dcc36cd58704f6eb392a54
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-16 20:18:33 +0330

    fix: adding header for supervision searching page

--- src/components/pages/dashboard/dashboardPages/supervision/tabs/SupervisionSearchingTabs.tsx
+++ src/components/pages/dashboard/dashboardPages/supervision/tabs/SupervisionSearchingTabs.tsx
@@ -4,22 +4,25 @@ import SearchByDay from "./parts/SearchByDay";
 
 const SupervisionSearchingTabs = () => {
   return (
-    <Tabs defaultValue="SearchByDay" className="">
-      <TabsList className="flex justify-center items-center bg-slate-300 !rounded-xl w-fit">
-        <TabsTrigger value="SearchByDay" className="data-[state=active]:bg-slate-50 !rounded-xl pt-2">
-          جستجو براساس روز
-        </TabsTrigger>
-        <TabsTrigger value="SearchByName" className="data-[state=active]:bg-slate-50 !rounded-xl pt-2">
-          جستجو براساس نام
-        </TabsTrigger>
-      </TabsList>
-      <TabsContent value="SearchByDay">
-        <SearchByDay />
-      </TabsContent>
-      <TabsContent value="SearchByName">
-        <SearchByName />
-      </TabsContent>
-    </Tabs>
+    <>
+      <h1 className="border-b-2 border-slate-300 w-fit font-bold text-xl mb-4">نظارت</h1>
+      <Tabs defaultValue="SearchByDay" className="">
+        <TabsList className="flex justify-center items-center bg-slate-300 !rounded-xl w-fit">
+          <TabsTrigger value="SearchByDay" className="data-[state=active]:bg-slate-50 !rounded-xl pt-2">
+            جستجو براساس روز
+          </TabsTrigger>
+          <TabsTrigger value="SearchByName" className="data-[state=active]:bg-slate-50 !rounded-xl pt-2">
+            جستجو براساس نام
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="SearchByDay">
+          <SearchByDay />
+        </TabsContent>
+        <TabsContent value="SearchByName">
+          <SearchByName />
+        </TabsContent>
+      </Tabs>
+    </>
   );
 };
 
