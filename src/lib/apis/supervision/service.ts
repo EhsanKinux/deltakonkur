40fee4908f9e7805a15ac65a7314b68048404ad2
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-16 19:17:20 +0330

    fix: api address changed

--- src/lib/apis/supervision/service.ts
+++ src/lib/apis/supervision/service.ts
@@ -8,7 +8,7 @@ export const get_students_by_name = ({ first_name, last_name }: { first_name: st
   );
 
 export const get_students_by_day = ({ solar_date_day }: { solar_date_day: string }) =>
-  fetchInstance(`api/register/student-advisors/?solar_date_day=${solar_date_day}`, { method: "GET" });
+  fetchInstance(`api/register/students/?solar_date_day=${solar_date_day}`, { method: "GET" });
 
 export const post_student_assassment = (body: IPostStudentAssessment) =>
   fetchInstance(`api/register/assessment/`, { method: "POST", body: JSON.stringify(body) });
