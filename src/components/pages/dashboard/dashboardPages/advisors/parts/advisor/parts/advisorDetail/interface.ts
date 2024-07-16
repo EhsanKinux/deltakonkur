8be84ceef3911cb92b025ae8cd9cc422802050a2
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-16 19:00:31 +0330

    feat: create new interfaces for advisor detail

--- src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/parts/advisorDetail/interface.ts
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/parts/advisorDetail/interface.ts
@@ -1,15 +1,49 @@
 import { FormEntry } from "../../../student/table/interfaces";
 
 export interface ProcessedStudentData extends FormEntry {
-    status: string | null;
-    id: string;
-    created: string;
-    started_date: string;
-    ended_date: string;
-  }
-  
+  status: string | null;
+  id: string;
+  created: string;
+  started_date: string;
+  ended_date: string;
+}
+
 export interface StudentInformation {
-    [key: string]: ProcessedStudentData | undefined;
-  }
-  
-  
+  [key: string]: ProcessedStudentData | undefined;
+}
+
+export interface Student {
+  id: string;
+  first_name: string;
+  last_name: string;
+  date_of_birth: string;
+  phone_number: string;
+  parent_phone: string;
+  home_phone: string;
+  school: string;
+  field: string;
+  grade: string;
+  created: string;
+  created_at: string;
+  solar_date_day: string | null;
+  solar_date_month: string | null;
+  solar_date_year: string | null;
+}
+
+export interface AdvisorDetailEntry {
+  id: string;
+  student: Student;
+  advisor: string;
+  status: string;
+  started_date: string;
+  ended_date: string | null;
+  solar_date_day: string | null;
+  solar_date_month: string | null;
+  solar_date_year: string | null;
+}
+
+export interface StudentWithDetails extends Student {
+  status: string;
+  started_date: string;
+  ended_date: string | null;
+}
