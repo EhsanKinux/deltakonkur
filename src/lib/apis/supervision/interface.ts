adc5e3ad417327763316494d0f8ee1809da07b10
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-16 11:51:24 +0330

    feat: create interface type for api data call

---
+++ src/lib/apis/supervision/interface.ts
@@ -0,0 +1,10 @@
+export interface IPostStudentAssessment {
+  student: string;
+  report_score: string;
+  phone_score: string;
+  advisor_behaviour_score: string;
+  followup_score: string;
+  motivation_score: string;
+  exam_score: string;
+  advisor_score: string;
+}
