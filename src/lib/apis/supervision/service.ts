c57e327a23458236f8ea69bedaef5fcfc6213c0a
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-16 11:51:55 +0330

    feat: create api call for posting assassment form

--- src/lib/apis/supervision/service.ts
+++ src/lib/apis/supervision/service.ts
@@ -1,4 +1,5 @@
 import { fetchInstance } from "../fetch-config";
+import { IPostStudentAssessment } from "./interface";
 
 export const get_students_by_name = ({ first_name, last_name }: { first_name: string; last_name: string }) =>
   fetchInstance(
@@ -8,3 +9,6 @@ export const get_students_by_name = ({ first_name, last_name }: { first_name: st
 
 export const get_students_by_day = ({ solar_date_day }: { solar_date_day: string }) =>
   fetchInstance(`api/register/student-advisors/?solar_date_day=${solar_date_day}`, { method: "GET" });
+
+export const post_student_assassment = (body: IPostStudentAssessment) =>
+  fetchInstance(`api/register/assessment/`, { method: "POST", body: JSON.stringify(body) });
