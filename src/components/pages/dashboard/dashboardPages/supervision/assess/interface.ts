310413855a03045203b7f86b3f5d2c2babb898d2
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-16 11:48:49 +0330

    feat: craete new data type interface for student assassment form

---
+++ src/components/pages/dashboard/dashboardPages/supervision/assess/interface.ts
@@ -0,0 +1,10 @@
+export interface IStudentAssessment {
+  student: string;
+  report_score: string;
+  phone_score: string;
+  advisor_behaviour_score: string;
+  followup_score: string;
+  motivation_score: string;
+  exam_score: string;
+  advisor_score: string;
+}
