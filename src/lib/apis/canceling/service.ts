584c3621c1e80154bdaa91914cb2e02d2b8a45c6
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-16 22:10:00 +0330

    feat: create student active check api get

--- src/lib/apis/canceling/service.ts
+++ src/lib/apis/canceling/service.ts
@@ -2,3 +2,6 @@ import { fetchInstance } from "../fetch-config";
 
 export const cancel_student = ({ studentId }: { studentId: string }) =>
   fetchInstance(`api/register/student-advisors/${studentId}/`, { method: "DELETE" });
+
+export const check_student_is_active = ({ studentId }: { studentId: string }) =>
+  fetchInstance(`api/register/active-student-advisor/${studentId}/`, { method: "GET" });
