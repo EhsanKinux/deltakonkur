4b06105992bbbe9470088816b5423d2e3d59862d
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-16 09:18:39 +0330

    create getting students by day query api

--- src/lib/apis/supervision/service.ts
+++ src/lib/apis/supervision/service.ts
@@ -5,3 +5,6 @@ export const get_students_by_name = ({ first_name, last_name }: { first_name: st
     `api/register/students/?first_name=${encodeURIComponent(first_name)}&last_name=${encodeURIComponent(last_name)}`,
     { method: "GET" }
   );
+
+export const get_students_by_day = ({ solar_date_day }: { solar_date_day: string }) =>
+  fetchInstance(`api/register/student-advisors/?solar_date_day=${solar_date_day}`, { method: "GET" });
