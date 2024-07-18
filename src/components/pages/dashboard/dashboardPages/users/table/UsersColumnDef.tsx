347406b6f7ef510db09814c89feb14a7e161710d
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-18 09:59:59 +0330

    feat: create column definition for users table

---
+++ src/components/pages/dashboard/dashboardPages/users/table/UsersColumnDef.tsx
@@ -0,0 +1,34 @@
+import { ColumnDef } from "@tanstack/react-table";
+import { IUsers } from "../users/interface";
+
+// import StudentDialogButtons from "./parts/StudentDialogButtons";
+
+export const userColumns: ColumnDef<IUsers>[] = [
+  {
+    accessorKey: "first_name",
+    header: "نام",
+  },
+  {
+    accessorKey: "last_name",
+    header: "نام خانوادگی",
+  },
+  {
+    accessorKey: "phone_number",
+    header: "شماره همراه",
+  },
+  {
+    accessorKey: "national_id",
+    header: "کد ملی",
+  },
+  {
+    accessorKey: "role",
+    header: "نوع کاربر",
+  },
+  //   {
+  //     id: "actions",
+  //     cell: ({ row }) => {
+  //       const formData = row.original;
+  //       return <StudentDialogButtons formData={formData} />;
+  //     },
+  //   },
+];
