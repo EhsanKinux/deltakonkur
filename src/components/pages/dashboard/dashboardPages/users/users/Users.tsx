e7f64cfff712e85036901d2e2a3d1433f31ce4e6
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-18 17:46:33 +0330

    fix: add header for users table page

--- src/components/pages/dashboard/dashboardPages/users/users/Users.tsx
+++ src/components/pages/dashboard/dashboardPages/users/users/Users.tsx
@@ -15,6 +15,7 @@ const Users = () => {
 
   return (
     <div className="flex flex-col">
+      <h1 className="border-b-2 border-slate-300 w-fit font-bold text-xl mb-4">همه‌ی کاربران</h1>
       <div className="flex flex-col justify-center items-center gap-3 p-16 mt-4 shadow-sidebar bg-slate-100 rounded-xl relative min-h-screen">
         <UsersTable columns={userColumns} data={users} />
       </div>
