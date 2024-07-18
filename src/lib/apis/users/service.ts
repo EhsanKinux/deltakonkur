e94b0b69063c9b9301e4fac574426bdd6741a1ff
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-18 17:48:05 +0330

    faet: create api call for deleting user, getting user information, and updating user information

--- src/lib/apis/users/service.ts
+++ src/lib/apis/users/service.ts
@@ -1,3 +1,4 @@
+import { IUserDetail } from "@/components/pages/dashboard/dashboardPages/users/userDetail/interface";
 import { fetchInstance } from "../fetch-config";
 import { ISubmitUserRegisteration } from "./interface";
 
@@ -5,3 +6,10 @@ export const get_all_users = () => fetchInstance(`api/auth/users/`, { method: "G
 
 export const submit_user_register_form = (body: ISubmitUserRegisteration) =>
   fetchInstance(`api/auth/users/`, { method: "POST", body: JSON.stringify(body) });
+
+export const delete_user = (userId: string) => fetchInstance(`api/auth/users/${userId}`, { method: "DELETE" });
+
+export const get_user_info = (userId: string) => fetchInstance(`api/auth/users/${userId}/`, { method: "GET" });
+
+export const update_user_info = (body: IUserDetail) =>
+  fetchInstance(`api/auth/users/${body.id}/`, { method: "PATCH", body: JSON.stringify(body) });
