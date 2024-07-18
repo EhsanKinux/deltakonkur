c5f7a901309189dee5921772d9eb6b194ff1356e
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-18 10:00:16 +0330

    feat: create user table

---
+++ src/components/pages/dashboard/dashboardPages/users/table/UsersTable.tsx
@@ -0,0 +1,102 @@
+import { flexRender, getCoreRowModel, useReactTable, getPaginationRowModel, ColumnDef } from "@tanstack/react-table";
+
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
+import { IUsers } from "../users/interface";
+
+interface UsersTableProps {
+  columns: ColumnDef<IUsers>[];
+  data: IUsers[];
+}
+
+export function UsersTable({ columns, data }: UsersTableProps) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    initialState: {
+      pagination: {
+        pageSize: 8,
+      },
+    },
+  });
+
+  const navigate = useNavigate();
+
+  const handleRowClick = (studentId: string) => {
+    // if (
+    //   (e.target as HTMLElement).tagName.toLowerCase() !== "button" &&
+    //   (e.target as HTMLElement).tagName.toLowerCase() !== "input"
+    // ) {
+    // }
+    navigate(`/dashboard/supervision/${studentId}`);
+  };
+
+  return (
+    <div className="w-full overflow-hidden p-10 absolute top-0 right-0 left-0 bottom-0">
+      <Table className="!rounded-xl border">
+        <TableHeader className="bg-slate-300">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id} className="!text-center">
+                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row, index) => (
+              <TableRow
+                className="hover:bg-slate-200 hover:cursor-pointer"
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+                onClick={() => handleRowClick(data[index].id)}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell className="!text-center" key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                نتیجه ای یافت نشد...
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+
+      <div className="flex items-center justify-between py-4 w-full">
+        <div className="flex gap-2">
+          <Button
+            className="rounded-[8px] border border-black"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            قبلی
+          </Button>
+          <Button
+            className="rounded-[8px] border border-black"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            بعدی
+          </Button>
+        </div>
+        <span className="text-slate-600 text-sm">
+          صفحه {table.getState().pagination.pageIndex + 1} از {table.getPageCount()}
+        </span>
+      </div>
+    </div>
+  );
+}
