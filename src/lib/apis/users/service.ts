9ea26879e47dc5857ab22f71ec2b8ed09602fb7b
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-18 12:09:16 +0330

    feat: create submit user api call for registering form

--- src/lib/apis/users/service.ts
+++ src/lib/apis/users/service.ts
@@ -1,3 +1,7 @@
 import { fetchInstance } from "../fetch-config";
+import { ISubmitUserRegisteration } from "./interface";
 
 export const get_all_users = () => fetchInstance(`api/auth/users/`, { method: "GET" });
+
+export const submit_user_register_form = (body: ISubmitUserRegisteration) =>
+  fetchInstance(`api/auth/users/`, { method: "POST", body: JSON.stringify(body) });
