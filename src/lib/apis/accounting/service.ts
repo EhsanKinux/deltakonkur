08ca8f3dc414dce8be4f2b05dce6a7a4c0153b5f
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-19 12:13:26 +0330

    feat: create new api data call

--- src/lib/apis/accounting/service.ts
+++ src/lib/apis/accounting/service.ts
@@ -1,3 +1,7 @@
 import { fetchInstance } from "../fetch-config";
 
 export const get_all_students = () => fetchInstance(`api/register/students/`, { method: "GET" });
+
+export const get_exel_info = () => fetchInstance(`api/register/accountant/`, { method: "GET" });
+
+export const get_exel_info_test = () => fetchInstance(`api/register/test-accountant/`, { method: "GET" });
