c3512b7968419d5bb1bba7f76bc7c8d06e23eb5b
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-19 17:57:57 +0330

    feat: create type interfaces for Exel data

---
+++ src/functions/hooks/accountingList/interface.ts
@@ -0,0 +1,15 @@
+export interface IJsonTestData {
+  first_name: string;
+  last_name: string;
+  to_account: string;
+  from_account: string;
+  amount: string;
+}[]
+
+export interface IJsonData {
+  first_name: string;
+  last_name: string;
+  to_account: string;
+  from_account: string;
+  amount: string;
+}[]
