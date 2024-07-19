445a32b06d4266e77285c0dbfac674e9ac3fb13a
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-19 12:09:57 +0330

    feat: create two buttons to generate json data and convert them in to Exel file with xlsx package

--- src/components/pages/dashboard/dashboardPages/accounting/allAdvisors/AllAccountingAdvisors.tsx
+++ src/components/pages/dashboard/dashboardPages/accounting/allAdvisors/AllAccountingAdvisors.tsx
@@ -1,9 +1,67 @@
-
-
+import { Button } from "@/components/ui/button";
+import { useAccounting } from "@/functions/hooks/accountingList/useAccounting";
+// import { utils, write } from "xlsx";
+// import { write } from "xlsx";
+import { saveAs } from "file-saver";
+import { useEffect } from "react";
 const AllAccountingAdvisors = () => {
-  return (
-    <div>AllAccountingAdvisors</div>
-  )
-}
+  const { getExelInfo, getTestExelInfo, jsonTestData, jsonData } = useAccounting();
 
-export default AllAccountingAdvisors
+  useEffect(() => {
+    if (jsonData && jsonData.length > 0) {
+      exportToExcel(jsonData, "Accountant.xlsx");
+    }
+  }, [jsonData, getExelInfo]);
+
+  useEffect(() => {
+    if (jsonTestData && jsonTestData.length > 0) {
+      exportToExcel(jsonTestData, "TestAccountant.xlsx");
+    }
+  }, [jsonTestData, getTestExelInfo]);
+
+  const handleExelData = async () => {
+    try {
+      await getExelInfo();
+      // jsonData will be updated, and useEffect will handle the export
+    } catch (error) {
+      console.error("Error fetching Exel data:", error);
+    }
+  };
+
+  const handleTestExelData = async () => {
+    try {
+      await getTestExelInfo();
+      // jsonTestData will be updated, and useEffect will handle the export
+    } catch (error) {
+      console.error("Error fetching test Exel data:", error);
+    }
+  };
+
+  const exportToExcel = async (data: any[], fileName: string | undefined) => {
+    const XLSX = await import("../allAdvisors/parts/EXEL/SheetJSWriteWrapper");
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+    const excelBuffer = XLSX.write(workbook, { bookType: "xlsx", type: "array" });
+    const dataBlob = new Blob([excelBuffer], {
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    });
+    saveAs(dataBlob, fileName);
+  };
+
+  return (
+    <div className="flex justify-center items-center gap-3 p-16 mt-4 shadow-sidebar bg-slate-100 rounded-xl">
+      <Button className="bg-blue-500 text-white hover:bg-blue-700 rounded-xl pt-2" onClick={handleTestExelData}>
+        گزارشگیری تستی
+      </Button>
+      <Button
+        className="bg-gray-300 text-black hover:bg-slate-700 hover:text-white rounded-xl pt-2"
+        onClick={handleExelData}
+      >
+        گزارشگیری
+      </Button>
+    </div>
+  );
+};
+
+export default AllAccountingAdvisors;
