4af970f8c9af1c87af69187ec15b6844394060d3
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-20 19:00:03 +0330

    feat: create and add all advisor necessary components

---
+++ src/components/pages/dashboard/dashboardPages/accounting/allAdvisors/allAccountingAdvisors/AllAccountingAdvisors.tsx
@@ -0,0 +1,14 @@
+import AccountantExel from "../accountantExel/AccountantExel";
+import AdvisorsList from "./parts/AdvisorsList";
+
+const AllAccountingAdvisors = () => {
+  return (
+    <div className="flex flex-col">
+      <h1 className="border-b-2 border-slate-300 w-fit font-bold text-xl">مشاوران</h1>
+      <AccountantExel />
+      <AdvisorsList />
+    </div>
+  );
+};
+
+export default AllAccountingAdvisors;
