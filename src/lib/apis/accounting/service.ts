e5269f6b1359224b69966ed75e1a153c0504bf2f
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-20 19:04:22 +0330

    feat: create getting advisor api fetch

--- src/lib/apis/accounting/service.ts
+++ src/lib/apis/accounting/service.ts
@@ -5,3 +5,5 @@ export const get_all_students = () => fetchInstance(`api/register/students/`, { 
 export const get_exel_info = () => fetchInstance(`api/register/accountant/`, { method: "GET" });
 
 export const get_exel_info_test = () => fetchInstance(`api/register/test-accountant/`, { method: "GET" });
+
+export const get_all_advisors = () => fetchInstance(`api/advisor/advisors/`, { method: "GET" });
