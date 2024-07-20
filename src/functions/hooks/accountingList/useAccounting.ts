1504f2ba0d9a207f4668b1dd767693a33d8e44d2
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-20 19:03:56 +0330

    feat: create getting advisor data info hook api call

--- src/functions/hooks/accountingList/useAccounting.ts
+++ src/functions/hooks/accountingList/useAccounting.ts
@@ -1,23 +1,25 @@
-import { get_all_students, get_exel_info, get_exel_info_test } from "@/lib/apis/accounting/service";
+import { get_all_advisors, get_all_students, get_exel_info, get_exel_info_test } from "@/lib/apis/accounting/service";
 import { accountingStore } from "@/lib/store/accountingStore";
-import { IallStudents } from "@/lib/store/types";
+import { IallAdvisors, IallStudents } from "@/lib/store/types";
 import { useCallback, useState } from "react";
 import { IJsonData, IJsonTestData } from "./interface";
 
 export const useAccounting = () => {
-  const [dataLoaded, setDataLoaded] = useState(false);
+  const [studentDataLoaded, setStudentDataLoaded] = useState(false);
+  const [advisorDataLoaded, setAdvisorDataLoaded] = useState(false);
   const [jsonTestData, setJsonTestData] = useState<IJsonTestData[]>();
   const [jsonData, setJsonData] = useState<IJsonData[]>();
 
   const addStudentData = accountingStore((state) => state.addAllstudents);
+  const addAlladvisors = accountingStore((state) => state.addAlladvisors);
 
   const getAllStudents = useCallback(async () => {
-    if (!dataLoaded) {
+    if (!studentDataLoaded) {
       const data = await get_all_students();
       data.forEach((student: IallStudents) => addStudentData(student));
-      setDataLoaded(true);
+      setStudentDataLoaded(true);
     }
-  }, [addStudentData, dataLoaded]);
+  }, [addStudentData, studentDataLoaded]);
 
   const getExelInfo = useCallback(async () => {
     const maindata = await get_exel_info();
@@ -31,5 +33,24 @@ export const useAccounting = () => {
     return maindata;
   }, []);
 
-  return { getAllStudents, getExelInfo, getTestExelInfo, jsonTestData, jsonData, setJsonTestData, setJsonData };
+  const getAdvisorsData = useCallback(async () => {
+    if (!advisorDataLoaded) {
+      const data = await get_all_advisors();
+      data.forEach((advisor: IallAdvisors) => {
+        addAlladvisors(advisor);
+      });
+      setAdvisorDataLoaded(true);
+    }
+  }, [addAlladvisors, advisorDataLoaded]);
+
+  return {
+    getAllStudents,
+    getExelInfo,
+    getTestExelInfo,
+    jsonTestData,
+    jsonData,
+    setJsonTestData,
+    setJsonData,
+    getAdvisorsData,
+  };
 };
