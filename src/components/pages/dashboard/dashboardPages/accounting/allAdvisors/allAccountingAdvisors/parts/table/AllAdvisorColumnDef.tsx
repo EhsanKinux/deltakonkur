27c543e7376908d6ac985ad1f83514ae4b90dac5
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-20 19:03:05 +0330

    feat: create advisro column definition for all advisors table

---
+++ src/components/pages/dashboard/dashboardPages/accounting/allAdvisors/allAccountingAdvisors/parts/table/AllAdvisorColumnDef.tsx
@@ -0,0 +1,38 @@
+import { IallAdvisors } from "@/lib/store/types";
+import { ColumnDef } from "@tanstack/react-table";
+
+
+export const advisorColumn: ColumnDef<IallAdvisors>[] = [
+  {
+    accessorKey: "first_name",
+    header: "نام",
+  },
+  {
+    accessorKey: "last_name",
+    header: "نام خانوادگی",
+  },
+  {
+    accessorKey: "phone_number",
+    header: "شماره همراه",
+  },
+  {
+    accessorKey: "national_id",
+    header: "کد ملی",
+  },
+  {
+    accessorKey: "field",
+    header: "رشته",
+  },
+  {
+    accessorKey: "bank_account",
+    header: "شماره حساب",
+  },
+//   {
+//     id: "actions",
+//     cell: ({ row }) => {
+//       const formData = row.original;
+
+//       return <AdvisorDialogButtons formData={formData} />;
+//     },
+//   },
+];
