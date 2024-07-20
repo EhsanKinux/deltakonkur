abdc1a5caf1b627122a276be75ce4c2bc95648d9
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-20 10:18:53 +0330

    feat: handle Exel exportation by using xlsx package

--- src/components/pages/dashboard/dashboardPages/accounting/allAdvisors/AllAccountingAdvisors.tsx
+++ src/components/pages/dashboard/dashboardPages/accounting/allAdvisors/AllAccountingAdvisors.tsx
@@ -1,94 +1,78 @@
+import { Button } from "@/components/ui/button";
+import { IJsonData } from "@/functions/hooks/accountingList/interface";
 import { useAccounting } from "@/functions/hooks/accountingList/useAccounting";
 import { convertToShamsi2 } from "@/lib/utils/date/convertDate";
-import { useEffect, useState } from "react";
-import { CSVLink } from "react-csv";
+import { useState } from "react";
 
 const AllAccountingAdvisors = () => {
-  const { getExelInfo, getTestExelInfo, jsonTestData, jsonData, setJsonTestData, setJsonData } = useAccounting();
+  const { getExelInfo, getTestExelInfo } = useAccounting();
 
   const [loading, setLoading] = useState(false);
-  const [testLoading, setTestLoading] = useState(false);
-  const [isModified, setIsModified] = useState(false);
-
-  const headers = [
-    { label: "نام", key: "first_name" },
-    { label: "نام خانوادگی", key: "last_name" },
-    { label: "به شماره حساب", key: "to_account" },
-    { label: "از شماره حساب", key: "from_account" },
-    { label: "مقدار", key: "amount" },
-  ];
 
   const getCurrentShamsiDate = () => {
     const today = new Date().toISOString();
     return convertToShamsi2(today);
   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        await getExelInfo();
-        await getTestExelInfo();
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setLoading(false);
-        setTestLoading(false);
+  const generateExcel = async (data: IJsonData[], filename: string) => {
+    const { utils, writeFile } = await import("../allAdvisors/parts/ExelXLSX/SheetJSWrapper");
+    const worksheet = utils.json_to_sheet(data);
+    const workbook = utils.book_new();
+    utils.book_append_sheet(workbook, worksheet, "Sheet1");
+    writeFile(workbook, filename);
+  };
+
+  const handleFetchAndGenerate = async (
+    fetchFunction: () => Promise<IJsonData[]>,
+    // setData: (data: IJsonData[]) => void,
+    filename: string
+  ) => {
+    try {
+      setLoading(true);
+      const data = await fetchFunction();
+      if (data && data.length > 0) {
+        // setData(data); // Update state if needed for future use
+        await generateExcel(data, filename);
+      } else {
+        console.warn("No data available to generate Excel file");
       }
-    };
-
-    fetchData();
-  }, [getExelInfo, getTestExelInfo]);
-
-  useEffect(() => {
-    if (jsonTestData && jsonTestData.length > 0 && !isModified) {
-      const modifiedData = jsonTestData.map((item) => ({
-        ...item,
-        to_account: `'${item.to_account}`,
-        from_account: `'${item.from_account}`,
-        amount: String(item.amount),
-      }));
-      setJsonTestData(modifiedData);
-      setIsModified(true);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false);
     }
-  }, [jsonTestData, isModified, setJsonTestData]);
-
-  useEffect(() => {
-    if (jsonData && jsonData.length > 0 && !isModified) {
-      const modifiedData = jsonData.map((item) => ({
-        ...item,
-        to_account: `'${item.to_account}`,
-        from_account: `'${item.from_account}`,
-        amount: String(item.amount),
-      }));
-      setJsonData(modifiedData);
-      setIsModified(true);
-    }
-  }, [jsonTestData, isModified, setJsonTestData]);
+  };
 
   return (
     <div className="flex justify-center items-center gap-3 p-16 mt-4 shadow-sidebar bg-slate-100 rounded-xl">
-      {!testLoading && (
-        <CSVLink
-          headers={headers}
-          data={jsonTestData || []}
-          filename={`TestAccountant_${getCurrentShamsiDate()}.csv`}
-          style={{ textDecoration: "none", color: "#fff" }}
+      {!loading && (
+        <Button
+          onClick={() => {
+            handleFetchAndGenerate(
+              getTestExelInfo,
+              // () => jsonTestData,
+              `TestAccountant_${getCurrentShamsiDate()}.xlsx`
+            );
+          }}
           className="bg-blue-500 text-white hover:bg-blue-700 rounded-xl p-2"
         >
           گزارشگیری تستی
-        </CSVLink>
+        </Button>
       )}
 
       {!loading && (
-        <CSVLink
-          headers={headers}
-          data={jsonData || []}
-          filename={`Accountant_${getCurrentShamsiDate()}.csv`}
-          style={{ textDecoration: "none", color: "#000" }}
+        <Button
+          onClick={() =>
+            handleFetchAndGenerate(
+              getExelInfo,
+              // () => jsonData,
+              `Accountant_${getCurrentShamsiDate()}.xlsx`
+            )
+          }
           className="bg-gray-300 text-black hover:bg-slate-700 hover:text-white rounded-xl p-2"
         >
           گزارشگیری
-        </CSVLink>
+        </Button>
       )}
     </div>
   );
