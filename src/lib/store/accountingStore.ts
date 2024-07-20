7259ce8a81dd6a4673dbb950e47d863b566393dc
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-20 19:04:55 +0330

    feat: create all advisors data state handling in zustand

--- src/lib/store/accountingStore.ts
+++ src/lib/store/accountingStore.ts
@@ -1,11 +1,13 @@
 import { create } from "zustand";
-import { IAccountStore, IallStudents } from "./types";
+import { IAccountStore, IallAdvisors, IallStudents } from "./types";
 import { convertToShamsi } from "../utils/date/convertDate";
 
 export const accountingStore = create<IAccountStore>((set) => ({
   allstudents: [],
+  alladvisors: [],
 
   setAllstudents: (students: IallStudents[]) => set({ allstudents: students }),
+  setAlladvisors: (advisors: IallAdvisors[]) => set({ alladvisors: advisors }),
 
   addAllstudents: (student: IallStudents) => {
     const convertedData = {
@@ -21,6 +23,16 @@ export const accountingStore = create<IAccountStore>((set) => ({
       }
     });
   },
+
+  addAlladvisors: (advisor: IallAdvisors) =>
+    set((state) => {
+      const advisorExists = state.alladvisors.some((existingAdvisor) => existingAdvisor.id === advisor.id);
+      if (!advisorExists) {
+        const updatedAdvisors = [...state.alladvisors, advisor];
+        return { alladvisors: updatedAdvisors };
+      }
+      return state;
+    }),
 
   deleteStudent: (studentId) =>
     set((state) => ({
