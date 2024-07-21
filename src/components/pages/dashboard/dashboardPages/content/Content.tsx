e195ed2e7e02a54fc637cb7e907fde0b2b9e7a32
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-21 12:38:58 +0330

    feat: create and initialize content page component

--- src/components/pages/dashboard/dashboardPages/content/Content.tsx
+++ src/components/pages/dashboard/dashboardPages/content/Content.tsx
@@ -1,9 +1,59 @@
-
+import { Form } from "@/components/ui/form";
+import { useAdvisorsList } from "@/functions/hooks/advisorsList/useAdvisorsList";
+import { contentFormSchema } from "@/lib/schema/Schema";
+import { appStore } from "@/lib/store/appStore";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import SearchAdvisors from "./parts/searchAdvisors/SearchAdvisors";
 
 const Content = () => {
-  return (
-    <div>Content</div>
-  )
-}
+  // const [isloading, setIsloading] = useState(false);
 
-export default Content
+  const {getAdvisorsData} = useAdvisorsList();
+  const advisors = appStore((state) => state.advisors);
+
+  useEffect(() => {
+    getAdvisorsData()
+  }, [getAdvisorsData])
+
+  const formSchema = contentFormSchema();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      // id: "",
+      first_name: "",
+      last_name: "",
+      content: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    // setIsloading(true);
+    if (data) {
+      console.table(data);
+    //   toast.promise(
+    //     submit_advisors_register_service(data).then(() => {
+    //       form.reset();
+    //     }),
+    //     {
+    //       loading: "در حال ثبت مشاور...",
+    //       success: "ثبت مشاور با موفقیت انجام شد!",
+    //       error: "شماره در سیستم موجود است!",
+    //     }
+    //   );
+    }
+    // setIsloading(false);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="mt-4 w-3/4 px-8">
+        <SearchAdvisors form={form} advisors={advisors} />
+      </form>
+    </Form>
+  );
+};
+
+export default Content;
