282dad1bca2014270e9dbc24b9e6c71d7b289708
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-21 12:29:26 +0330

    fix: filter sidebar base on the user roles

--- src/components/pages/dashboard/dashboardLayout/parts/sidebar/Sidebar.tsx
+++ src/components/pages/dashboard/dashboardLayout/parts/sidebar/Sidebar.tsx
@@ -4,9 +4,28 @@ import { cn } from "@/lib/utils/cn/cn";
 import { Link, useLocation } from "react-router-dom";
 import logOutIcon from "@/assets/icons/logout.svg";
 import LinkWithChildren from "./parts/LinkWithChilderen";
+import { authStore } from "@/lib/store/authStore";
 
 const Sidebar = () => {
   const location = useLocation();
+  const { userRole } = authStore();
+
+  const filterLinksByRole = (links: any, role: number | null) => {
+    return links
+      .filter((link) => link.roles.includes(role))
+      .map((link) => {
+        if (link.children) {
+          return {
+            ...link,
+            children: filterLinksByRole(link.children, role),
+          };
+        }
+        return link;
+      });
+  };
+
+  const filteredLinks = filterLinksByRole(sidebarLinks, userRole);
+
   return (
     <section className="min-w-[100px] max-h-screen right-0 top-0 bottom-0 flex flex-col justify-between  bg-black pt-8 text-white max-md:hidden sm:p-4 xl:p-6 lg:w-[200px] 2xl:w-[240px] m-5 rounded-xl overflow-hidden">
       <nav className="flex flex-col gap-4">
@@ -27,28 +46,28 @@ const Sidebar = () => {
           <div className="h-10 absolute bottom-0 w-full bg-gradient-to-t from-black to-transparent z-10"></div>
           <div className="h-10 absolute top-0 w-full bg-gradient-to-b from-black to-transparent z-10"></div>
           <div className="relative flex flex-col gap-3 overflow-y-auto no-scrollbar h-[60vh] py-10">
-          {sidebarLinks.map((item) => {
-            const isActive = location.pathname === item.route || location.pathname.endsWith(`${item.route}/`);
-            return item.children ? (
-              <LinkWithChildren key={item.id} menuItem={item} isActive={isActive} />
-            ) : (
-              <Link to={item.route} key={item.label} className={cn("sidebar-link", { "bg-gray-200": isActive })}>
-                <div className="relative size-6">
-                  <img
-                    src={item.imgURL}
-                    alt={item.label}
-                    className={cn({ "brightness-[3] invert-0": isActive, "slate-100-color": !isActive })}
-                    style={{
-                      filter: isActive
-                        ? "none"
-                        : "invert(100%) sepia(0%) saturate(0%) hue-rotate(180deg) brightness(1) contrast(1)",
-                    }}
-                  />
-                </div>
-                <p className={cn("sidebar-label", { "!text-gray-900": isActive })}>{item.label}</p>
-              </Link>
-            );
-          })}
+            {filteredLinks.map((item: any) => {
+              const isActive = location.pathname === item.route || location.pathname.endsWith(`${item.route}/`);
+              return item.children ? (
+                <LinkWithChildren key={item.id} menuItem={item} isActive={isActive} />
+              ) : (
+                <Link to={item.route} key={item.label} className={cn("sidebar-link", { "bg-gray-200": isActive })}>
+                  <div className="relative size-6">
+                    <img
+                      src={item.imgURL}
+                      alt={item.label}
+                      className={cn({ "brightness-[3] invert-0": isActive, "slate-100-color": !isActive })}
+                      style={{
+                        filter: isActive
+                          ? "none"
+                          : "invert(100%) sepia(0%) saturate(0%) hue-rotate(180deg) brightness(1) contrast(1)",
+                      }}
+                    />
+                  </div>
+                  <p className={cn("sidebar-label", { "!text-gray-900": isActive })}>{item.label}</p>
+                </Link>
+              );
+            })}
           </div>
         </div>
       </nav>
@@ -56,7 +75,7 @@ const Sidebar = () => {
       <div className="flex flex-col overflow-y-auto no-scrollbar rounded-xl  border-2 border-slate-500 justify-center">
         <Link to="/dashboard" className={cn("sidebar-link")}>
           <div className="relative size-6">
-            <img src={logOutIcon} alt={"خروج از حساب"} className={cn("brightness-[100] invert-0")}/>
+            <img src={logOutIcon} alt={"خروج از حساب"} className={cn("brightness-[100] invert-0")} />
           </div>
           <p className="text-slate-300">خروج</p>
         </Link>
