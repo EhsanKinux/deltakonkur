11b9d1e340cb101998427ba4a0b230f47cffdd11
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-21 17:22:45 +0330

    feat: get the name of the user base on the role

--- src/components/pages/dashboard/dashboardLayout/parts/Header/DHeader.tsx
+++ src/components/pages/dashboard/dashboardLayout/parts/Header/DHeader.tsx
@@ -1,14 +1,36 @@
+import { authStore } from "@/lib/store/authStore";
 import MobileNav from "./parts/Mobile/MobileNav";
 import UserAccoutn from "./parts/UserAccoutn";
+import { useAuth } from "@/lib/apis/authentication/useAuth";
+import { useEffect, useState } from "react";
+import { IUserDetail } from "../../../dashboardPages/users/userDetail/interface";
 
 export default function DHeader() {
+  const { userRole } = authStore();
+  const { fetchUserData } = useAuth();
+  const [user, setUser] = useState<IUserDetail>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (String(userRole)) {
+        const userdata = await fetchUserData();
+        setUser(userdata);
+      }
+    };
+
+    fetchData();
+  }, [fetchUserData, userRole]);
+
+  // console.log(userRole);
   return (
     <div className="p-8 w-full border-b-2 border-slate-300 h-10 flex justify-between items-center mt-5 mr-0 ml-8">
       <div className="flex gap-2 items-center">
         <MobileNav />
-        <span className="text-black text-nowrap">سلام کاربر</span>
+        <span className="text-black text-nowrap font-light">
+          سلام {user?.first_name} {user?.last_name}
+        </span>
       </div>
-      <UserAccoutn />
+      <UserAccoutn user={user} />
     </div>
   );
 }
