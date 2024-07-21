9577884abe5f2f055084d96d2ee233b12995809f
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-21 12:38:07 +0330

    feat: getting detail of advisor when the user is advisor with role 7

--- src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/parts/advisorDetail/AdvisorDetail.tsx
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/parts/advisorDetail/AdvisorDetail.tsx
@@ -10,18 +10,60 @@ import { convertToShamsi } from "@/lib/utils/date/convertDate";
 import { AdvisorDitailTable } from "../../../table/AdvisorDitailTable";
 import { stColumns } from "./parts/advisorStudentTable/ColumnDef";
 import { AdvisorDetailEntry, StudentWithDetails } from "./interface";
+import { authStore } from "@/lib/store/authStore";
+import axios from "axios";
+import { BASE_API_URL } from "@/lib/variables/variables";
+
+export interface ICurrentUser {
+  id: number;
+  first_name: string;
+  last_name: string;
+  national_id: string;
+  phone_number: string;
+  role: number;
+}
 
 const AdvisorDetail = () => {
   const { advisorId } = useParams();
+  const { accessToken, userRole, setUserRole } = authStore();
   const navigate = useNavigate();
   const { advisorDetailData, getStudentsOfAdvisor } = useAdvisorsList();
   const [processedStudentData, setProcessedStudentData] = useState<StudentWithDetails[]>([]);
+  const [currentUser, setCurrentUser] = useState<ICurrentUser>();
+  // const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (userRole === 7) {
+      const fetchUserData = async () => {
+        try {
+          const roleResponse = await axios.get(`${BASE_API_URL}/api/auth/current-user/`, {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          });
+          const userData = roleResponse.data;
+          setCurrentUser(userData); // Set the user information
+          setUserRole(userData.role); // Update the user role in the store
+        } catch (error) {
+          console.error("Error fetching user data:", error);
+          navigate("/unauthorized"); // Redirect if there is an error fetching user data
+        } finally {
+          // setLoading(false);
+        }
+      };
+      fetchUserData();
+    }
+  }, [navigate, userRole, accessToken, setUserRole]);
+  console.table(currentUser);
 
   useEffect(() => {
     if (advisorId) {
       getStudentsOfAdvisor(advisorId);
     }
-  }, [advisorId, getStudentsOfAdvisor]);
+    if (userRole === 7) {
+      getStudentsOfAdvisor(String(currentUser?.id));
+    }
+  }, [advisorId, currentUser?.id, getStudentsOfAdvisor, userRole]);
 
   useEffect(() => {
     if (advisorDetailData) {
@@ -39,6 +81,9 @@ const AdvisorDetail = () => {
 
   const goToAdisors = () => {
     navigate("/dashboard/advisors");
+    if (userRole === 7) {
+      navigate("/dashboard");
+    }
   };
 
   if (!advisorId) {
