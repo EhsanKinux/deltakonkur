7c2de10c09dfaaf6417aa97385a66e0a142db573
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-22 12:35:35 +0330

    feat: create interface type for assessments

---
+++ src/functions/hooks/supervision/interface.ts
@@ -0,0 +1,12 @@
+export interface IAssessments {
+  id: string;
+  student: string;
+  plan_score: string;
+  report_score: string;
+  phone_score: string;
+  advisor_behaviour_score: string;
+  followup_score: string;
+  motivation_score: string;
+  exam_score: string;
+  advisor_score: string;
+}
