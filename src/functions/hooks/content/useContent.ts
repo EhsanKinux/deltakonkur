7427954ca2b8c6fe8035461e90d3f65119a14fc4
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-23 11:28:38 +0330

    feat: create getting advisor content function api call in hook

--- src/functions/hooks/content/useContent.ts
+++ src/functions/hooks/content/useContent.ts
@@ -1,10 +1,13 @@
 import { IContent } from "@/lib/apis/content/interface";
-import { send_content } from "@/lib/apis/content/service";
+import { get_advisor_content, send_content } from "@/lib/apis/content/service";
 import { useCallback, useState } from "react";
+import { IAdvisorContent } from "./interface";
+import { convertToShamsi } from "@/lib/utils/date/convertDate";
 
 export const useContent = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+  const [advisorContent, setAdvisorContent] = useState<IAdvisorContent[]>([]);
 
   const sendContent = useCallback(
     async (body: IContent[]) => {
@@ -16,8 +19,7 @@ export const useContent = () => {
           //   const updatedInfo = await response.json();
           //   setStudentInfo(updatedInfo);
           console.log(response);
-        }
-        else {
+        } else {
           setError("Failed to update student information");
         }
       } catch (err) {
@@ -33,5 +35,38 @@ export const useContent = () => {
     [setError, setLoading]
   );
 
-  return { sendContent, loading, error };
+  const getAdvisorContent = useCallback(
+    async (advisorId: string) => {
+      setLoading(true);
+      setError("");
+      try {
+        const response = await get_advisor_content(advisorId);
+        const data: IAdvisorContent[] = await response;
+        // Transform the is_delivered field
+        const transformedData = data.map((item) => ({
+          ...item,
+          is_delivered: item.is_delivered ? "بله" : "خیر",
+          delivered_at: convertToShamsi(item.delivered_at),
+          created: convertToShamsi(item.created),
+        }));
+        setAdvisorContent(transformedData);
+        if (response.ok) {
+          console.log("");
+        } else {
+          setError("Failed to fetch advisor content");
+        }
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError("Failed to fetch advisor content");
+        }
+      } finally {
+        setLoading(false);
+      }
+    },
+    [setError, setLoading]
+  );
+
+  return { sendContent, loading, error, getAdvisorContent, advisorContent };
 };
