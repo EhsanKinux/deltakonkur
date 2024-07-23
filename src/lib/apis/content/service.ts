3c1a0614e7a55771abba707e3199b8ceb40eb152
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-23 11:29:06 +0330

    feat: create fetching advisor content

--- src/lib/apis/content/service.ts
+++ src/lib/apis/content/service.ts
@@ -3,3 +3,6 @@ import { IContent } from "./interface";
 
 export const send_content = (body: IContent[]) =>
   fetchInstance(`api/content/contents/`, { method: "POST", body: JSON.stringify(body) });
+
+export const get_advisor_content = (advisorId: string) =>
+  fetchInstance(`api/content/advisor-contents/${advisorId}/`, { method: "GET" });
