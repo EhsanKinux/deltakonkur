fd61455a013cc31e6d655b63ca45edb047d1c6c1
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-23 17:44:53 +0330

    fix: unauthorized back button condition

--- src/components/pages/auth/Unauthorized.tsx
+++ src/components/pages/auth/Unauthorized.tsx
@@ -1,12 +1,17 @@
+import { authStore } from "@/lib/store/authStore";
 import { Link } from "react-router-dom";
 
 const Unauthorized = () => {
+  const { userRole } = authStore();
   return (
     <div className="flex flex-col w-full h-screen justify-center items-center gap-4">
       <h1>دسترسی مجاز نیست!</h1>
       <p>به این صفحه دسترسی ندارید!</p>
-      <Link to="/dashboard" className="p-5 bg-yellow-600 text-slate-100 rounded-xl font-semibold">
-        بازگشت به صفحه ی اصلی
+      <Link
+        to={userRole ? "/dashboard" : "/auth/signIn"}
+        className="p-5 bg-yellow-600 text-slate-100 rounded-xl font-semibold"
+      >
+        {userRole ? "بازگشت به صفحه ی اصلی" : "بازگشت به صفحه ورود"}
       </Link>
     </div>
   );
