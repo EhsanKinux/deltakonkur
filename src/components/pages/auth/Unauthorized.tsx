a54deffa95cb2519c1e8408a76fe5bd76bcdddf4
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-23 11:53:37 +0330

    fix: unauthorized page style

--- src/components/pages/auth/Unauthorized.tsx
+++ src/components/pages/auth/Unauthorized.tsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Unauthorized = () => {
   return (
-    <div>
-      <h1>Unauthorized</h1>
-      <p>You do not have permission to view this page.</p>
+    <div className="flex flex-col w-full h-screen justify-center items-center gap-4">
+      <h1>دسترسی مجاز نیست!</h1>
+      <p>به این صفحه دسترسی ندارید!</p>
+      <Link to="/dashboard" className="p-5 bg-yellow-600 text-slate-100 rounded-xl font-semibold">
+        بازگشت به صفحه ی اصلی
+      </Link>
     </div>
   );
 };
