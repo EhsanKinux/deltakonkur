0828beebe68934d4cb74fafb5e09ae471aeecd5f
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-23 11:15:56 +0330

    feat: create advisor detail content table

---
+++ src/components/pages/dashboard/dashboardPages/content/advisors/parts/contentAdvisorDetail/parts/table/AdvisorDetailContentTable.tsx
@@ -0,0 +1,139 @@
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  getPaginationRowModel,
+  getFilteredRowModel,
+  ColumnFiltersState,
+} from "@tanstack/react-table";
+
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+//   import { useNavigate } from "react-router-dom";
+// import { Input } from "@/components/ui/input";
+import { useState } from "react";
+// import SearchIcon from "@/assets/icons/search.svg";
+import { IAdvisorContent } from "@/functions/hooks/content/interface";
+
+interface AdvisorDetailContentTableProps {
+  columns: ColumnDef<IAdvisorContent>[];
+  data: IAdvisorContent[];
+}
+
+export function AdvisorDetailContentTable({ columns, data }: AdvisorDetailContentTableProps) {
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    initialState: {
+      pagination: {
+        pageSize: 8,
+      },
+    },
+    state: {
+      columnFilters,
+    },
+  });
+
+  // const navigate = useNavigate();
+
+  // const handleRowClick = (advisorId: string, e: React.MouseEvent) => {
+  //   if (
+  //     (e.target as HTMLElement).tagName.toLowerCase() !== "button" &&
+  //     (e.target as HTMLElement).tagName.toLowerCase() !== "input"
+  //   ) {
+  //     navigate(`/dashboard/content/advisors/${advisorId}`);
+  //   }
+  // };
+
+  return (
+    <div className="w-full overflow-hidden p-10 absolute top-0 right-0 left-0 bottom-0">
+      {/* <div className="flex flex-col items-center xl:flex-row gap-2 py-4">
+        <div className="relative flex items-center w-full text-14 rounded-[8px]">
+          <img src={SearchIcon} alt="searchicon" className="absolute left-3 w-6 h-6 text-gray-500" />
+          <Input
+            placeholder="جستجو براساس نام"
+            value={(table.getColumn("first_name")?.getFilterValue() as string) ?? ""}
+            onChange={(event) => table.getColumn("first_name")?.setFilterValue(event.target.value)}
+            className="text-16 placeholder:text-16 rounded-[8px] text-gray-900 border-slate-400 placeholder:text-gray-500 hover:placeholder:text-blue-500 hover:cursor-pointer"
+          />
+        </div>
+        <div className="relative flex items-center w-full text-14 rounded-[8px]">
+          <img src={SearchIcon} alt="searchicon" className="absolute left-3 w-6 h-6 text-gray-500" />
+          <Input
+            placeholder="جستجو براساس نام خانوادگی"
+            value={(table.getColumn("last_name")?.getFilterValue() as string) ?? ""}
+            onChange={(event) => table.getColumn("last_name")?.setFilterValue(event.target.value)}
+            className="text-16 placeholder:text-16 rounded-[8px] text-gray-900 border-slate-400 placeholder:text-gray-500 hover:placeholder:text-blue-500"
+          />
+        </div>
+      </div> */}
+      <Table className="!rounded-xl border mt-5">
+        <TableHeader className="bg-slate-300">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id} className="!text-center">
+                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                className="hover:bg-slate-200 hover:cursor-pointer"
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+                //   onClick={(e) => handleRowClick(data[index].id, e)}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell className="!text-center" key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                نتیجه ای یافت نشد...
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+      <div className="flex items-center justify-between py-4 w-full">
+        <div className="flex gap-2">
+          <Button
+            className="rounded-[8px] border border-black"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            قبلی
+          </Button>
+          <Button
+            className="rounded-[8px] border border-black"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            بعدی
+          </Button>
+        </div>
+        <span className="text-slate-600 text-sm">
+          صفحه {table.getState().pagination.pageIndex + 1} از {table.getPageCount()}
+        </span>
+      </div>
+    </div>
+  );
+}
