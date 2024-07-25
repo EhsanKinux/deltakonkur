34e86305abbdff8c96cdc53c5950478f316bf7ca
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-25 15:46:52 +0330

    feat: create separate select custom  component for advisors student edit

---
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/parts/advisorDetail/parts/advisorStudentTable/parts/fieldAndGrade/SelectCustom.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+
+const SelectCustom = React.forwardRef<HTMLButtonElement, any>((props, ref) => {
+  return (
+    <Select {...props} disabled>
+      <SelectTrigger
+        ref={ref}
+        className="w-full text-16 placeholder:text-16 rounded-[8px] text-gray-500 border-slate-400"
+      >
+        <SelectValue placeholder={props.placeholder} />
+      </SelectTrigger>
+      <SelectContent className="bg-slate-100 rounded-xl shadow-lg mt-2">
+        {props.options.map((option: { value: string; label: string }) => (
+          <SelectItem key={option.value} value={option.value}>
+            {option.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+});
+
+SelectCustom.displayName = "SelectCustom";
+
+export default SelectCustom;
