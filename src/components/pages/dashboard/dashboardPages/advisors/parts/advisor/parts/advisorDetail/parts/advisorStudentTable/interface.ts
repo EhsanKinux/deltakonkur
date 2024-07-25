047dd306e37b87806acfb31656594fefa9ca146c
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-25 15:44:26 +0330

    feat: create edit advisor student interface

---
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/parts/advisorDetail/parts/advisorStudentTable/interface.ts
@@ -0,0 +1,14 @@
+export interface IEditAdvisorStudents {
+  id: string;
+  date_of_birth: string;
+  first_name: string;
+  last_name: string;
+  school: string;
+  phone_number: string;
+  home_phone: string;
+  parent_phone: string;
+  field: string;
+  grade: string;
+  created: string;
+  advisor: string;
+}
