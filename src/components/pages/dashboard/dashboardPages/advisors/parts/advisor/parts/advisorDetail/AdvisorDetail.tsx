7ce3ef1cdc6e513e3ce76f590ee7aa08b1618717
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-26 16:48:15 +0330

    fix: showing the advisor info base on the role of 7

--- src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/parts/advisorDetail/AdvisorDetail.tsx
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/parts/advisorDetail/AdvisorDetail.tsx
@@ -25,13 +25,29 @@ export interface ICurrentUser {
   role: number;
 }
 
+export interface AdvisorData {
+  id: number;
+  first_name: string;
+  last_name: string;
+  field: string;
+  phone_number: string;
+  national_id: string;
+  bank_account: string;
+  created: string;
+  updated: string;
+  active_students: number;
+  stopped_students: number;
+  cancelled_students: number;
+}
+
 const AdvisorDetail = () => {
   const { advisorId } = useParams();
   const { accessToken, userRole, setUserRole } = authStore();
   const navigate = useNavigate();
   const { advisorDetailData, getStudentsOfAdvisor } = useAdvisorsList();
   const [processedStudentData, setProcessedStudentData] = useState<StudentWithDetails[]>([]);
-  const [currentUser, setCurrentUser] = useState<ICurrentUser>();
+  // const [currentUser, setCurrentUser] = useState<ICurrentUser>();
+  const [advisorData, setAdvisorData] = useState<AdvisorData | null>(null);
   // const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -44,13 +60,19 @@ const AdvisorDetail = () => {
             },
           });
           const userData = roleResponse.data;
-          setCurrentUser(userData); // Set the user information
+          // setCurrentUser(userData); // Set the user information
           setUserRole(userData.role); // Update the user role in the store
+
+          // Fetch advisor data based on user ID
+          const advisorResponse = await axios.get(`${BASE_API_URL}/api/advisor/advisor/from-user/${userData.id}/`, {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          });
+          setAdvisorData(advisorResponse.data); // Set the advisor data
         } catch (error) {
-          console.error("Error fetching user data:", error);
+          console.error("Error fetching user/advisor data:", error);
           navigate("/unauthorized"); // Redirect if there is an error fetching user data
-        } finally {
-          // setLoading(false);
         }
       };
       fetchUserData();
@@ -62,10 +84,10 @@ const AdvisorDetail = () => {
     if (advisorId) {
       getStudentsOfAdvisor(advisorId);
     }
-    if (userRole === 7) {
-      getStudentsOfAdvisor(String(currentUser?.id));
+    if (userRole === 7 && advisorData) {
+      getStudentsOfAdvisor(String(advisorData?.id));
     }
-  }, [advisorId, currentUser?.id, getStudentsOfAdvisor, userRole]);
+  }, [advisorData, advisorId, getStudentsOfAdvisor, userRole]);
 
   useEffect(() => {
     if (advisorDetailData) {
@@ -102,7 +124,7 @@ const AdvisorDetail = () => {
         <img className="w-5 pb-[2px]" src={backIcon} alt="backIcon" />
         <span>بازگشت</span>
       </Button>
-      <AdvisorInfo advisorId={advisorId} />
+      <AdvisorInfo advisorId={advisorId} advisorData={advisorData} userRole={userRole} />
       <Tabs defaultValue="studentTable" className="mt-4">
         <TabsList className="flex justify-center items-center bg-slate-300 !rounded-xl w-fit">
           <TabsTrigger value="studentTable" className="data-[state=active]:bg-slate-50 !rounded-xl pt-2">
