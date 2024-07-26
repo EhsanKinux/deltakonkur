e7f62e4e13027b90d42cb26d23e597ed0f606103
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-26 20:12:28 +0330

    fix: unnecessary prop

--- src/components/pages/auth/sign-in/SignIn.tsx
+++ src/components/pages/auth/sign-in/SignIn.tsx
@@ -4,7 +4,7 @@ import AuthForm from "../parts/form/AuthForm";
 const SignIn = () => {
   return (
     <section className="flex gap-2 justify-center items-center">
-      <AuthForm type="sign-in" />
+      <AuthForm />
     </section>
   );
 };
