d146e5e6cf94cba7a274bd57886498accf5e1505
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-26 13:20:16 +0330

    feat: create new interfaces for accounting data handling

--- src/components/pages/dashboard/dashboardPages/accounting/allStudents/parts/interfaces.ts
+++ src/components/pages/dashboard/dashboardPages/accounting/allStudents/parts/interfaces.ts
@@ -10,3 +10,61 @@ export type IAccountingStudents = {
   grade: string;
   created: string;
 };
+
+// Interface for the student object
+export interface IStudent {
+  id: string;
+  first_name: string;
+  last_name: string;
+  date_of_birth: string;
+  phone_number: string;
+  parent_phone: string;
+  home_phone: string;
+  school: string;
+  field: string;
+  grade: string;
+  created: string;
+  created_at: string;
+  solar_date_day: string;
+  solar_date_month: string;
+  solar_date_year: string;
+}
+
+// Interface for the main student-advisor object
+export interface IStudentAdvisor {
+  // id: string;
+  student: IStudent;
+  advisor: string;
+  status: string;
+  started_date: string;
+  ended_date: string;
+  solar_date_day: string;
+  solar_date_month: string;
+  solar_date_year: string;
+  expire_date: string;
+  left_days_to_expire: string;
+  stop_date: string;
+}
+
+export interface IFormattedStudentAdvisor {
+  id: string; // ID for the whole data
+  studentId: string; // ID from the student object
+  grade: string;
+  advisor: string;
+  created: string;
+  expire_date: string;
+  left_days_to_expire: string;
+  first_name: string;
+  last_name: string;
+  date_of_birth: string;
+  phone_number: string;
+  parent_phone: string;
+  home_phone: string;
+  school: string;
+  field: string;
+  created_at: string;
+  solar_date_day: string;
+  solar_date_month: string;
+  solar_date_year: string;
+  stop_date: string;
+}
