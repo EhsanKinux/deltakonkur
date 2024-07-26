e75d53924288cb3c52aa5e3df6f256ebf3cd0269
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-26 13:24:07 +0330

    feat: create getting student with advisor, stop student, and restart student api fetch functions

--- src/lib/apis/accounting/service.ts
+++ src/lib/apis/accounting/service.ts
@@ -1,4 +1,5 @@
 import { fetchInstance } from "../fetch-config";
+import { IRestartStudent, IStopStudent } from "./interface";
 
 export const get_all_students = () => fetchInstance(`api/register/students/`, { method: "GET" });
 
@@ -7,3 +8,14 @@ export const get_exel_info = () => fetchInstance(`api/register/accountant/`, { m
 export const get_exel_info_test = () => fetchInstance(`api/register/test-accountant/`, { method: "GET" });
 
 export const get_all_advisors = () => fetchInstance(`api/advisor/advisors/`, { method: "GET" });
+
+export const get_students_with_advisors = () => fetchInstance(`api/register/student-advisors/`, { method: "GET" });
+
+export const stop_student_advisor = (body: IStopStudent) =>
+  fetchInstance(`api/register/student-advisors/`, { method: "PATCH", body: JSON.stringify(body) });
+
+export const reset_student = (body: IRestartStudent) =>
+  fetchInstance(`api/register/student-advisors/restart/${body.student}/`, {
+    method: "POST",
+    body: JSON.stringify(body),
+  });
