f4ee28be6b97b7146c21f177cef3218fd27f490b
EhsanKINUX <ehsankhwentao@gmail.com>
2024-07-28 18:45:07 +0330

    feat: create content delivered confirmation fetch api

--- src/lib/apis/content/service.ts
+++ src/lib/apis/content/service.ts
@@ -1,8 +1,11 @@
 import { fetchInstance } from "../fetch-config";
-import { IContent } from "./interface";
+import { IContent, IDelivered } from "./interface";
 
 export const send_content = (body: IContent[]) =>
   fetchInstance(`api/content/contents/`, { method: "POST", body: JSON.stringify(body) });
 
 export const get_advisor_content = (advisorId: string) =>
   fetchInstance(`api/content/advisor-contents/${advisorId}/`, { method: "GET" });
+
+export const send_content_delivered = ({ body }: { body: IDelivered }) =>
+  fetchInstance(`api/content/contents/partial_edit/${body.id}/`, { method: "PATCH", body: JSON.stringify(body) });
