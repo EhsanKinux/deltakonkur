35e10c044db0d969147bf365683d5e4e19d01831
EhsanKINUX <ehsankhwentao@gmail.com>
2024-08-03 00:05:14 +0330

    fix: student assessments input fields and form toast message handling

--- src/components/pages/dashboard/dashboardPages/supervision/assess/StudentAssessment.tsx
+++ src/components/pages/dashboard/dashboardPages/supervision/assess/StudentAssessment.tsx
@@ -10,14 +10,17 @@ import { Button } from "@/components/ui/button";
 import { Loader2 } from "lucide-react";
 import backIcon from "@/assets/icons/back.svg";
 import { useStudentList } from "@/functions/hooks/studentsList/useStudentList";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import RecentAssassments from "./parts/recentAssassments/RecentAssassments";
+import { toast } from "sonner";
 
 const StudentAssessment = () => {
   const { studentId } = useParams();
-  const { submitAssassmentForm, loading } = useSupervision();
+  const { submitAssassmentForm } = useSupervision();
   const { fetchStudentInfo, studentInfo } = useStudentList();
   const navigate = useNavigate();
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   useEffect(() => {
     if (studentId) {
@@ -55,9 +58,22 @@ const StudentAssessment = () => {
         exam_score: data.exam_score,
         advisor_score: data.advisor_score,
       };
-      if (dataTransformed) {
+      try {
+        setIsSubmitting(true);
         console.table(dataTransformed);
-        await submitAssassmentForm(dataTransformed).then(() => form.reset());
+        const loadingToastId = toast.loading("در حال ثبت نظرسنجی...");
+        await submitAssassmentForm(dataTransformed);
+        form.reset();
+        toast.dismiss(loadingToastId);
+        toast.success("نظرسنجی با موفقیت ثبت شد!");
+        setTimeout(() => {
+          window.location.reload();
+        }, 2000);
+      } catch (error) {
+        toast.dismiss(); // Dismiss any active toast
+        toast.error("خطایی در ثبت نظرسنجی رخ داده است!");
+      } finally {
+        setIsSubmitting(false);
       }
     }
   };
@@ -90,52 +106,68 @@ const StudentAssessment = () => {
               name="plan_score"
               label="نمره برنامه‌ریزی"
               placeHolder="یک عدد بین 0 تا 10 وارد کنید..."
+              min={0}
+              max={10}
             />
             <CustomInputAssassment
               control={form.control}
               name="report_score"
               label="نمره گزارش کار"
               placeHolder="یک عدد بین 0 تا 10 وارد کنید..."
+              min={0}
+              max={10}
             />
             <CustomInputAssassment
               control={form.control}
               name="phone_score"
               label="نمره تایم تماس تلفنی"
               placeHolder="یک عدد بین 0 تا 10 وارد کنید..."
+              min={0}
+              max={10}
             />
             <CustomInputAssassment
               control={form.control}
               name="advisor_behaviour_score"
               label="نمره برخورد مشاور"
               placeHolder="یک عدد بین 0 تا 10 وارد کنید..."
+              min={0}
+              max={10}
             />
             <CustomInputAssassment
               control={form.control}
               name="followup_score"
               label="نمره پیگیری و جدیت"
               placeHolder="یک عدد بین 0 تا 10 وارد کنید..."
+              min={0}
+              max={10}
             />
             <CustomInputAssassment
               control={form.control}
               name="motivation_score"
               label="نمره عملکرد انگیزشی"
               placeHolder="یک عدد بین 0 تا 10 وارد کنید..."
+              min={0}
+              max={10}
             />
             <CustomInputAssassment
               control={form.control}
               name="exam_score"
               label="نمره آزمون"
               placeHolder="یک عدد بین 0 تا 4 وارد کنید..."
+              min={0}
+              max={4}
             />
             <CustomInputAssassment
               control={form.control}
               name="advisor_score"
               label="نمره کلی مشاوره"
               placeHolder="یک عدد بین 0 تا 20 وارد کنید..."
+              min={0}
+              max={20}
             />
             <div className="flex flex-col justify-center items-center w-full mt-5">
-              <Button type="submit" className="form-btn w-full hover:bg-blue-800">
-                {loading ? (
+              <Button type="submit" className="form-btn w-full hover:bg-blue-800" disabled={isSubmitting}>
+                {isSubmitting ? (
                   <>
                     <Loader2 size={20} className="animate-spin" />
                     &nbsp; در حال ثبت...
--- src/components/pages/dashboard/dashboardPages/supervision/assess/parts/customInput/CustomInputAssassment.tsx
+++ src/components/pages/dashboard/dashboardPages/supervision/assess/parts/customInput/CustomInputAssassment.tsx
@@ -1,12 +1,27 @@
 import { FormControl, FormField, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Control } from "react-hook-form";
+import { ChangeEvent } from "react";
+import { Control, ControllerRenderProps } from "react-hook-form";
+
+interface FormValues {
+  student: string;
+  plan_score: string;
+  report_score: string;
+  phone_score: string;
+  advisor_behaviour_score: string;
+  followup_score: string;
+  motivation_score: string;
+  exam_score: string;
+  advisor_score: string;
+}
 
 const CustomInputAssassment = ({
   control,
   name,
   label,
   placeHolder,
+  min,
+  max,
 }: {
   control: Control<
     {
@@ -33,7 +48,19 @@ const CustomInputAssassment = ({
     | "plan_score";
   label: string;
   placeHolder: string;
+  min: number;
+  max: number;
 }) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>, field: ControllerRenderProps<FormValues, typeof name>) => {
+    const value = e.target.value;
+    const numberValue = parseFloat(value);
+
+    // Allow empty input or numbers within the specified range
+    if (value === "" || (!isNaN(numberValue) && numberValue >= min && numberValue <= max)) {
+      field.onChange(value); // Update form value only if valid
+    }
+  };
+
   return (
     <FormField
       control={control}
@@ -47,7 +74,10 @@ const CustomInputAssassment = ({
               className="text-16 placeholder:text-16 rounded-[8px] text-gray-900 border-slate-400 placeholder:text-gray-400"
               type="text"
               placeholder={placeHolder}
-              {...field}
+              onChange={(e) => handleChange(e, field)}
+              value={field.value} // Use the value from field
+              onBlur={field.onBlur} // Use the onBlur from field
+              ref={field.ref} // Use the ref from field
             />
           </FormControl>
           <FormMessage className="form-message mt-2" />
