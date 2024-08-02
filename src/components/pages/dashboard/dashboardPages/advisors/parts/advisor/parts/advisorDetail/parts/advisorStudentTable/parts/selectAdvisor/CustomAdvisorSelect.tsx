bfd424b5b67987135e3266c464c3de07bec3430e
EhsanKINUX <ehsankhwentao@gmail.com>
2024-08-02 17:00:42 +0330

    fix: search advisor input in advisor detail

--- src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/parts/advisorDetail/parts/advisorStudentTable/parts/selectAdvisor/CustomAdvisorSelect.tsx
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/parts/advisorDetail/parts/advisorStudentTable/parts/selectAdvisor/CustomAdvisorSelect.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
 
-const CustomAdvisorSelect = React.forwardRef<HTMLButtonElement, any>((props, ref) => {
+type OptionType = {
+  value: string;
+  label: string;
+};
+
+interface CustomAdvisorSelectProps {
+  options: OptionType[];
+  placeholder: string;
+  value: string;
+  onValueChange: (value: string) => void;
+}
+
+const CustomAdvisorSelect = React.forwardRef<HTMLButtonElement, CustomAdvisorSelectProps>((props, ref) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const filteredOptions = props.options.filter((option) => option.label.includes(searchTerm));
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [searchTerm]);
+
+  // Function to prevent default touch behavior
+  const preventTouchClose = (event: React.TouchEvent | React.MouseEvent) => {
+    event.stopPropagation();
+  };
+
   return (
-    <Select {...props}>
+    <Select
+      {...props}
+      onOpenChange={(open) => {
+        // Focus the input when the dropdown opens
+        if (open && inputRef.current) {
+          inputRef.current.focus();
+        }
+      }}
+    >
       <SelectTrigger
         ref={ref}
         className="w-full text-16 placeholder:text-16 rounded-[8px] text-gray-500 border-slate-400"
@@ -12,7 +49,18 @@ const CustomAdvisorSelect = React.forwardRef<HTMLButtonElement, any>((props, ref
       </SelectTrigger>
       <SelectContent className="bg-slate-100 rounded-xl shadow-lg mt-2">
         <SelectGroup>
-          {props.options.map((option: { value: string; label: string }) => (
+          <Input
+            ref={inputRef}
+            type="text"
+            placeholder="جستجو..."
+            className="w-full p-2 rounded-xl border border-slate-300"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            onTouchStart={preventTouchClose}
+            onTouchEnd={preventTouchClose}
+            onMouseDown={preventTouchClose}
+          />
+          {filteredOptions.map((option) => (
             <SelectItem key={option.value} value={option.value}>
               {option.label}
             </SelectItem>
