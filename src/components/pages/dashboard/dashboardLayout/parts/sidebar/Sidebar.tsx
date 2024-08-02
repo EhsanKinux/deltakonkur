17851833dabfd1445161e960d727b42ebe87068b
Sina Moradian <sinacsharp@gmail.com>
2024-08-02 16:35:35 +0330

    Fix: Fix scrollbar styles on big screens.

--- src/components/pages/dashboard/dashboardLayout/parts/sidebar/Sidebar.tsx
+++ src/components/pages/dashboard/dashboardLayout/parts/sidebar/Sidebar.tsx
@@ -22,7 +22,10 @@ const Sidebar = () => {
   const { userRole } = authStore();
   const clearAuth = authStore((state) => state.clearAuth);
 
-  const filterLinksByRole = (links: SidebarLink[], role: number | null): SidebarLink[] => {
+  const filterLinksByRole = (
+    links: SidebarLink[],
+    role: number | null
+  ): SidebarLink[] => {
     if (role === null) {
       return [];
     }
@@ -47,36 +50,48 @@ const Sidebar = () => {
   };
 
   return (
-    <section className="min-w-[100px] max-h-screen right-0 top-0 bottom-0 flex flex-col justify-between  bg-black pt-8 text-white max-md:hidden sm:p-4 xl:p-6 lg:w-[200px] 2xl:w-[240px] m-5 rounded-xl overflow-hidden">
-      <nav className="flex flex-col gap-4">
-        <Link to="/dashboard" className="flex mb-12 cursor-pointer items-center gap-2 justify-center p-10">
-          <img
-            src={FakeLogo}
-            width={34}
-            height={34}
-            alt="Kinux-Logo"
-            className={cn("brightness-[100] invert-0 size-[24px] max-xl:size-14")}
-          />
+    <section className="min-w-[100px] max-h-screen right-0 top-0 bottom-0 flex flex-col justify-between bg-black pt-8 text-white max-md:hidden sm:p-4 xl:p-6 md:w-[240px] m-5 rounded-xl overflow-hidden">
+      <nav className="h-4/5 flex flex-col gap-4">
+        {/* logo */}
+        <div className="h-1/6 max-h-[60px] flex flex-col justify-center">
+          <Link
+            to="/dashboard"
+            className="flex cursor-pointer items-center gap-2 justify-center"
+          >
+            <h1 className="sidebar-logo text-xl">دلتا کنکور</h1>
+          </Link>
+        </div>
 
-          <h1 className="sidebar-logo text-xl">دلتا کنکور</h1>
-        </Link>
         {/* nav links */}
-        <div className="relative flex flex-col gap-3 overflow-y-auto no-scrollbar  overflow-hidden max-h-[700px]">
+        <div className="flex-1 relative flex flex-col gap-3 overflow-y-auto overflow-hidden scroll">
           {/* scroll shadow */}
           <div className="h-10 absolute bottom-0 w-full bg-gradient-to-t from-black to-transparent z-10"></div>
           <div className="h-10 absolute top-0 w-full bg-gradient-to-b from-black to-transparent z-10"></div>
-          <div className="relative flex flex-col gap-3 overflow-y-auto no-scrollbar h-[60vh] py-10">
+          <div className="relative flex flex-col gap-3 overflow-y-auto max-h-[68vh] no-scrollbar py-4">
             {filteredLinks.map((item: SidebarLink) => {
-              const isActive = location.pathname === item.route || location.pathname.endsWith(`${item.route}/`);
+              const isActive =
+                location.pathname === item.route ||
+                location.pathname.endsWith(`${item.route}/`);
               return item.children ? (
-                <LinkWithChildren key={item.id} menuItem={item} isActive={isActive} />
+                <LinkWithChildren
+                  key={item.id}
+                  menuItem={item}
+                  isActive={isActive}
+                />
               ) : (
-                <Link to={item.route} key={item.label} className={cn("sidebar-link", { "bg-gray-200": isActive })}>
+                <Link
+                  to={item.route}
+                  key={item.label}
+                  className={cn("sidebar-link", { "bg-gray-200": isActive })}
+                >
                   <div className="relative size-6">
                     <img
                       src={item.imgURL}
                       alt={item.label}
-                      className={cn({ "brightness-[3] invert-0": isActive, "slate-100-color": !isActive })}
+                      className={cn({
+                        "brightness-[3] invert-0": isActive,
+                        "slate-100-color": !isActive,
+                      })}
                       style={{
                         filter: isActive
                           ? "none"
@@ -84,7 +99,13 @@ const Sidebar = () => {
                       }}
                     />
                   </div>
-                  <p className={cn("sidebar-label", { "!text-gray-900": isActive })}>{item.label}</p>
+                  <p
+                    className={cn("sidebar-label", {
+                      "!text-gray-900": isActive,
+                    })}
+                  >
+                    {item.label}
+                  </p>
                 </Link>
               );
             })}
@@ -92,10 +113,14 @@ const Sidebar = () => {
         </div>
       </nav>
       {/* footer */}
-      <div className="flex flex-col overflow-y-auto no-scrollbar rounded-xl  border-2 border-slate-500 justify-center">
+      <div className="h-1/5 max-h-[60px] flex flex-col overflow-y-auto no-scrollbar rounded-xl  border-2 border-slate-500 justify-center">
         <Button className={cn("sidebar-link")} onClick={handleLogout}>
           <div className="relative size-6">
-            <img src={logOutIcon} alt={"خروج از حساب"} className={cn("brightness-[100] invert-0")} />
+            <img
+              src={logOutIcon}
+              alt={"خروج از حساب"}
+              className={cn("brightness-[100] invert-0")}
+            />
           </div>
           <p className="text-slate-300">خروج</p>
         </Button>
