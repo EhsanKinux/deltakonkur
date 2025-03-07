406eb1eb2a6cf9f440c188d2c0dca6a40bfe17e8
Sina Moradian <sinacsharp@gmail.com>
2025-03-08 01:40:27 +0330

    Reformat pagination: AdvisorList in Advisors page

--- src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/AdvisorList.tsx
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/advisor/AdvisorList.tsx
@@ -1,101 +1,89 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { useAdvisorsList } from "@/functions/hooks/advisorsList/useAdvisorsList";
-import { appStore } from "@/lib/store/appStore";
-import { Advisor } from "@/lib/store/types";
-import { useEffect, useMemo } from "react";
+import { BASE_API_URL } from "@/lib/variables/variables";
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { AdvisorDataTable } from "../table/AdvisorDataTable";
 import { columns } from "./parts/table/ColumnDef";
 
 const AdvisorList = () => {
-  const { getAdvisorsData } = useAdvisorsList();
-  const advisors = appStore((state) => state.advisors);
-
-  // Use search params to manage query string
+  const [advisors, setAdvisors] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
+  const [totalPages, setTotalPages] = useState("");
 
-  // Extract tab from query params or set default
   const activeTab = searchParams.get("tab") || "mathAdvisors";
+  const getAdvisors = useCallback(async () => {
+    const field =
+      searchParams.get("tab") === "mathAdvisors"
+        ? "ریاضی"
+        : searchParams.get("tab") === "experimentalAdvisors"
+        ? "تجربی"
+        : "علوم انسانی";
+
+    const page = searchParams.get("page") || 1;
+    const firstName = searchParams.get("first_name") || "";
+    const lastName = searchParams.get("last_name") || "";
+
+    try {
+      const { data } = await axios.get(`${BASE_API_URL}api/advisor/advisors/`, {
+        params: {
+          field,
+          page: page,
+          first_name: firstName,
+          last_name: lastName,
+        },
+      });
+
+      setAdvisors(data.results);
+      setTotalPages(Number(data.count / 10).toFixed(0));
+    } catch (error) {
+      console.error("خطا در دریافت اطلاعات مشاوران:", error);
+    }
+  }, [searchParams, setAdvisors]);
 
   useEffect(() => {
-    if (!searchParams.has("tab")) {
-      // Set the default tab if it's not present in the URL
-      setSearchParams({ tab: "mathAdvisors" });
-    }
-    getAdvisorsData();
-  }, [getAdvisorsData, searchParams, setSearchParams]);
+    getAdvisors();
+  }, [getAdvisors, searchParams]);
 
   const handleTabChange = (value: string) => {
-    setSearchParams({ tab: value });
+    setSearchParams({ tab: value, page: "1" });
+    getAdvisors();
   };
-
-  const calculateActivePercentage = (active: number, stopped: number, canceled: number) => {
-    const total = active + stopped + canceled;
-    return total ? ((active / total) * 100).toFixed(2) : "0.00";
-  };
-
-  const memoizedAdvisors: Advisor[] = useMemo(() => {
-    const levelMapping: { [key: string]: string } = {
-      "1": "سطح 1",
-      "2": "سطح 2",
-      "3": "سطح 3",
-      "4": "ارشد 1",
-      "5": "ارشد 2",
-    };
-
-    return advisors.map((advisor) => ({
-      ...advisor,
-      activePercentage: parseFloat(
-        calculateActivePercentage(
-          parseInt(advisor.active_students ?? "0"),
-          parseInt(advisor.stopped_students ?? "0"),
-          parseInt(advisor.cancelled_students ?? "0")
-        )
-      ),
-      level: levelMapping[advisor.level.toString()] || advisor.level,
-    }));
-  }, [advisors]);
-
-  const sortAdvisorsByActivePercentage = (advisors: Advisor[]): Advisor[] => {
-    return advisors.sort((a, b) => (b.activePercentage ?? 0) - (a.activePercentage ?? 0));
-  };
-  const mathAdvisors = sortAdvisorsByActivePercentage(memoizedAdvisors.filter((advisor) => advisor.field === "ریاضی"));
-  const experimentalAdvisors = sortAdvisorsByActivePercentage(
-    memoizedAdvisors.filter((advisor) => advisor.field === "تجربی")
-  );
-  const humanitiesAdvisors = sortAdvisorsByActivePercentage(
-    memoizedAdvisors.filter((advisor) => advisor.field === "علوم انسانی")
-  );
 
   return (
-    <section className="max-h-screen">
-      <h1 className="border-b-2 border-slate-300 w-fit font-bold text-xl">مشاوران</h1>
+    <section className="">
+      <h1 className="border-b-2 border-slate-300 w-fit font-bold text-xl">
+        مشاوران
+      </h1>
 
       <Tabs value={activeTab} onValueChange={handleTabChange} className="mt-4">
         <TabsList className="flex justify-center items-center bg-slate-300 !rounded-xl w-fit">
-          <TabsTrigger value="mathAdvisors" className="data-[state=active]:bg-slate-50 !rounded-xl pt-2">
+          <TabsTrigger
+            value="mathAdvisors"
+            className="data-[state=active]:bg-slate-50 !rounded-xl pt-2"
+          >
             ریاضی
           </TabsTrigger>
-          <TabsTrigger value="experimentalAdvisors" className="data-[state=active]:bg-slate-50 !rounded-xl pt-2">
+          <TabsTrigger
+            value="experimentalAdvisors"
+            className="data-[state=active]:bg-slate-50 !rounded-xl pt-2"
+          >
             تجربی
           </TabsTrigger>
-          <TabsTrigger value="humanitiesAdvisors" className="data-[state=active]:bg-slate-50 !rounded-xl pt-2">
+          <TabsTrigger
+            value="humanitiesAdvisors"
+            className="data-[state=active]:bg-slate-50 !rounded-xl pt-2"
+          >
             علوم انسانی
           </TabsTrigger>
         </TabsList>
-        <TabsContent value="mathAdvisors">
-          <div className="flex flex-col justify-center items-center gap-3 mt-4 shadow-sidebar bg-slate-100 rounded-xl relative min-h-screen">
-            <AdvisorDataTable columns={columns} data={mathAdvisors} />
-          </div>
-        </TabsContent>
-        <TabsContent value="experimentalAdvisors">
-          <div className="flex flex-col justify-center items-center gap-3 mt-4 shadow-sidebar bg-slate-100 rounded-xl relative min-h-screen">
-            <AdvisorDataTable columns={columns} data={experimentalAdvisors} />
-          </div>
-        </TabsContent>
-        <TabsContent value="humanitiesAdvisors">
-          <div className="flex flex-col justify-center items-center gap-3 mt-4 shadow-sidebar bg-slate-100 rounded-xl relative min-h-screen">
-            <AdvisorDataTable columns={columns} data={humanitiesAdvisors} />
+        <TabsContent value={activeTab}>
+          <div className="flex flex-col justify-center items-center gap-3 mt-4 bg-slate-100 rounded-xl relative min-h-[120vh]">
+            <AdvisorDataTable
+              columns={columns}
+              data={advisors}
+              totalPages={totalPages}
+            />
           </div>
         </TabsContent>
       </Tabs>
--- src/components/pages/dashboard/dashboardPages/advisors/parts/table/AdvisorDataTable.tsx
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/table/AdvisorDataTable.tsx
@@ -1,158 +1,151 @@
-import SearchIcon from "@/assets/icons/search.svg";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { Advisor } from "@/lib/store/types";
 import {
-  ColumnDef,
-  ColumnFiltersState,
-  flexRender,
-  getCoreRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
-import { useEffect, useState } from "react";
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Advisor } from "@/lib/store/types";
+import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 interface AdvisorDataTableProps {
-  columns: ColumnDef<Advisor>[];
+  columns: any[];
   data: Advisor[];
+  isLoading: boolean;
+  totalPages: string;
 }
 
-export function AdvisorDataTable({ columns, data }: AdvisorDataTableProps) {
-  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+export function AdvisorDataTable({
+  columns,
+  data,
+  isLoading,
+  totalPages,
+}: AdvisorDataTableProps) {
   const navigate = useNavigate();
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const [pagination, setPagination] = useState({
-    pageIndex: Number(queryParams.get("page")) || 0,
-    pageSize: 8,
-  });
 
-  const table = useReactTable({
-    data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    onColumnFiltersChange: setColumnFilters,
-    getFilteredRowModel: getFilteredRowModel(),
-    onPaginationChange: setPagination,
-    state: {
-      pagination,
-      columnFilters,
-    },
-    autoResetPageIndex: false,
-  });
+  const page = Number(queryParams.get("page")) || 1;
+  const firstName = queryParams.get("first_name") || "";
+  const lastName = queryParams.get("last_name") || "";
 
-  useEffect(() => {
-    const updateQueryParam = () => {
-      const newPage = table.getState().pagination.pageIndex;
-      const params = new URLSearchParams(location.search);
-      params.set("page", newPage.toString());
-      navigate(`?${params.toString()}`, { replace: true });
-    };
-    if (location.search) {
-      updateQueryParam();
+  const [isTableLoading, setIsTableLoading] = useState(false);
+
+  const handleSearch = (column: string, value: string) => {
+    const params = new URLSearchParams(location.search);
+    if (value) {
+      params.set(column, value);
+    } else {
+      params.delete(column);
     }
-  }, [table.getState().pagination.pageIndex, navigate, location.search]);
-
-  const handleRowClick = (advisorId: string, e: React.MouseEvent) => {
-    if (
-      (e.target as HTMLElement).tagName.toLowerCase() !== "button" &&
-      (e.target as HTMLElement).tagName.toLowerCase() !== "input"
-    ) {
-      navigate(`/dashboard/advisors/${advisorId}`);
-    }
+    params.set("page", "1");
+    navigate(`?${params.toString()}`, { replace: true });
   };
+
+  const handlePageChange = (newPage: number) => {
+    if (newPage < 1 || newPage > totalPages) return;
+
+    setIsTableLoading(true);
+    const params = new URLSearchParams(location.search);
+    params.set("page", newPage.toString());
+    navigate(`?${params.toString()}`, { replace: true });
+
+    setTimeout(() => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+      setIsTableLoading(false);
+    }, 500);
+  };
+
   return (
     <div className="w-full overflow-auto p-10 absolute top-0 right-0 left-0 bottom-0">
       <div className="flex flex-col items-center xl:flex-row gap-2 py-4">
-        <div className="relative flex items-center w-full text-14 rounded-[8px]">
-          <img src={SearchIcon} alt="searchicon" className="absolute left-3 w-6 h-6 text-gray-500" />
-          <Input
-            placeholder="جستجو براساس نام"
-            value={(table.getColumn("first_name")?.getFilterValue() as string) ?? ""}
-            onChange={(event) => {
-              table.setPageIndex(0);
-              table.getColumn("first_name")?.setFilterValue(event.target.value);
-            }}
-            className="placeholder:text-14 placeholder:text-gray-500 rounded-[8px] text-gray-900 border-slate-400 hover:placeholder:text-blue-500 hover:cursor-pointer"
-          />
-        </div>
-        <div className="relative flex items-center w-full text-14 rounded-[8px]">
-          <img src={SearchIcon} alt="searchicon" className="absolute left-3 w-6 h-6 text-gray-500" />
-          <Input
-            placeholder="جستجو براساس نام خانوادگی"
-            value={(table.getColumn("last_name")?.getFilterValue() as string) ?? ""}
-            onChange={(event) => {
-              table.setPageIndex(0);
-              table.getColumn("last_name")?.setFilterValue(event.target.value);
-            }}
-            className="placeholder:text-14 placeholder:text-gray-500 rounded-[8px] text-gray-900 border-slate-400 hover:placeholder:text-blue-500 hover:cursor-pointer"
-          />
-        </div>
+        <Input
+          placeholder="جستجو براساس نام"
+          value={firstName}
+          onChange={(e) => handleSearch("first_name", e.target.value)}
+          className="placeholder:text-14 rounded-[8px] border-slate-400"
+        />
+        <Input
+          placeholder="جستجو براساس نام خانوادگی"
+          value={lastName}
+          onChange={(e) => handleSearch("last_name", e.target.value)}
+          className="placeholder:text-14 rounded-[8px] border-slate-400"
+        />
       </div>
       <Table className="!rounded-xl border mt-5">
         <TableHeader className="bg-slate-300">
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id} className="!text-center">
-                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-                  </TableHead>
-                );
-              })}
-            </TableRow>
-          ))}
+          <TableRow>
+            {columns.map((col) => (
+              <TableHead key={col.id} className="!text-center">
+                {col.header}
+              </TableHead>
+            ))}
+          </TableRow>
         </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                className="hover:bg-slate-200 hover:cursor-pointer"
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-                onClick={(e) => handleRowClick(row.original.id, e)}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell className="!text-center" key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))
-          ) : (
+        {isLoading || isTableLoading ? (
+          <TableBody>
             <TableRow>
               <TableCell colSpan={columns.length} className="h-24 text-center">
-                نتیجه ای یافت نشد...
+                ⏳ در حال بارگذاری...
               </TableCell>
             </TableRow>
-          )}
-        </TableBody>
+          </TableBody>
+        ) : (
+          <TableBody>
+            {data.length ? (
+              data.map((row) => (
+                <TableRow
+                  key={row.id}
+                  className="hover:bg-slate-200 hover:cursor-pointer transition-all duration-300"
+                >
+                  {columns.map((col) => (
+                    <TableCell key={col.id} className="!text-center">
+                      {row[col.accessorKey]}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  نتیجه‌ای یافت نشد...
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        )}
       </Table>
-      <div className="flex items-center justify-between py-4 w-full">
-        <div className="flex gap-2">
+      {totalPages ? (
+        <div className="flex items-center justify-between py-4 w-full">
           <Button
             className="rounded-[8px] border border-black"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
+            onClick={() => handlePageChange(page - 1)}
+            disabled={page <= 1}
           >
             قبلی
           </Button>
+          <span className="text-slate-600 text-sm">
+            صفحه {page} از {totalPages}
+          </span>
+
           <Button
             className="rounded-[8px] border border-black"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
+            onClick={() => handlePageChange(page + 1)}
+            disabled={page >= totalPages}
           >
             بعدی
           </Button>
         </div>
-        <span className="text-slate-600 text-sm">
-          صفحه {table.getState().pagination.pageIndex + 1} از {table.getPageCount()}
-        </span>
-      </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
