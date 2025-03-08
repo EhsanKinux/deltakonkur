036022321007ddbe716b8b56515bcb85a5e7faa5
Sina Moradian <sinacsharp@gmail.com>
2025-03-08 03:58:10 +0330

    Reformat pagination: all students list in advisors page

--- src/components/pages/dashboard/dashboardPages/advisors/parts/student/AllStudentsList.tsx
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/student/AllStudentsList.tsx
@@ -1,21 +1,84 @@
-import { useAccounting } from "@/functions/hooks/accountingList/useAccounting";
-import { accountingStore } from "@/lib/store/accountingStore";
-import { useEffect } from "react";
+import { BASE_API_URL } from "@/lib/variables/variables";
+import axios from "axios";
+import { debounce } from "lodash";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import { AllStudentsDataTable } from "../table/AllStudentsDataTable";
 import { stColumns } from "./tabs/parts/AllstudentColumnDef";
 
 const AllStudentsList = () => {
-  const allstudents = accountingStore((state) => state.allstudents);
-  const { getAllStudents } = useAccounting();
+  const [students, setStudents] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [totalPages, setTotalPages] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const abortControllerRef = useRef<AbortController | null>(null); // اضافه کردن abortController
+
+  const getStudents = useCallback(async () => {
+    // اگر ریکوئست قبلی وجود داشت، کنسل کن
+    if (abortControllerRef.current) {
+      abortControllerRef.current.abort();
+    }
+
+    abortControllerRef.current = new AbortController();
+    const signal = abortControllerRef.current.signal;
+    const page = searchParams.get("page") || 1;
+    const firstName = searchParams.get("first_name") || "";
+    const lastName = searchParams.get("last_name") || "";
+    const field = searchParams.get("field") || "";
+    const grade = searchParams.get("grade") || "";
+    const solarDateDay = searchParams.get("solar_date_day") || "";
+
+    try {
+      setIsLoading(true);
+      const { data } = await axios.get(
+        `${BASE_API_URL}api/register/students-no-advisor/`,
+        {
+          params: {
+            page,
+            first_name: firstName,
+            last_name: lastName,
+            solar_date_day: solarDateDay,
+            grade: grade,
+            field: field,
+          },
+          signal, // ارسال سیگنال
+        }
+      );
+
+      setStudents(data.results);
+      setTotalPages(Number(data.count / 10).toFixed(0));
+    } catch (error: any) {
+      if (axios.isCancel(error)) {
+        console.log("🔴 درخواست لغو شد");
+      } else {
+        console.error("خطا در دریافت اطلاعات دانش آموزان:", error);
+      }
+    }
+    setIsLoading(false);
+  }, [searchParams, setStudents]);
+
+  // Debounce کردن تابع getStudents
+  const debouncedgetStudents = useCallback(debounce(getStudents, 50), [
+    getStudents,
+  ]);
 
   useEffect(() => {
-    getAllStudents();
-  }, [getAllStudents]);
+    debouncedgetStudents();
+    return () => {
+      debouncedgetStudents.cancel();
+    };
+  }, [searchParams]);
 
   return (
     <section className="max-h-screen">
       <div className="flex flex-col justify-center items-center gap-3 p-16 mt-4 shadow-sidebar bg-slate-100 rounded-xl relative min-h-screen">
-        <AllStudentsDataTable columns={stColumns} data={allstudents} />
+        <AllStudentsDataTable
+          columns={stColumns}
+          data={students}
+          totalPages={totalPages}
+          isLoading={isLoading}
+        />
       </div>
     </section>
   );
--- src/components/pages/dashboard/dashboardPages/advisors/parts/table/AllStudentsDataTable.tsx
+++ src/components/pages/dashboard/dashboardPages/advisors/parts/table/AllStudentsDataTable.tsx
@@ -8,7 +8,14 @@ import {
   getFilteredRowModel,
 } from "@tanstack/react-table";
 
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
@@ -18,153 +25,180 @@ import { useLocation, useNavigate } from "react-router-dom";
 interface AllStudentsDataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
+  isLoading: boolean;
+  totalPages: string;
 }
 
-export function AllStudentsDataTable<TData, TValue>({ columns, data }: AllStudentsDataTableProps<TData, TValue>) {
-  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+// کامپوننت Skeleton برای نمایش در حالت لودینگ
+const SkeletonRow = ({ columnsCount }: { columnsCount: number }) => {
+  return (
+    <TableRow>
+      {Array.from({ length: columnsCount }).map((_, index) => (
+        <TableCell key={index} className="!text-center">
+          <div className="h-5 w-20 bg-gray-300 animate-pulse rounded-xl mx-auto"></div>
+        </TableCell>
+      ))}
+    </TableRow>
+  );
+};
+
+export function AllStudentsDataTable<TData, TValue>({
+  columns,
+  data,
+  isLoading,
+  totalPages,
+}: AllStudentsDataTableProps<TData, TValue>) {
   const navigate = useNavigate();
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const [pagination, setPagination] = useState({
-    pageIndex: Number(queryParams.get("page")) || 0,
-    pageSize: 8,
-  });
 
-  const table = useReactTable({
-    data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    onColumnFiltersChange: setColumnFilters,
-    getFilteredRowModel: getFilteredRowModel(),
-    onPaginationChange: setPagination,
-    state: {
-      pagination,
-      columnFilters,
-    },
-    autoResetPageIndex: false,
-  });
+  const page = Number(queryParams.get("page")) || 1;
+  const firstName = queryParams.get("first_name") || "";
+  const lastName = queryParams.get("last_name") || "";
+  const field = queryParams.get("field") || "";
+  const grade = queryParams.get("grade") || "";
+
+  const [isTableLoading, setIsTableLoading] = useState(false);
+
+  const handleSearch = (key: string, value: string) => {
+    const params = new URLSearchParams(location.search);
+    if (value) {
+      params.set(key, value);
+    } else {
+      params.delete(key);
+    }
+    params.set("page", "1");
+    navigate(`?${params.toString()}`, { replace: true });
+  };
+
+  const handlePageChange = (newPage: number) => {
+    if (newPage < 1 || newPage > Number(totalPages)) return;
+
+    setIsTableLoading(true);
+    const params = new URLSearchParams(location.search);
+    params.set("page", newPage.toString());
+    navigate(`?${params.toString()}`, { replace: true });
+
+    setTimeout(() => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+      setIsTableLoading(false);
+    }, 500);
+  };
 
   useEffect(() => {
-    const updateQueryParam = () => {
-      const newPage = table.getState().pagination.pageIndex;
-      const params = new URLSearchParams(location.search);
-      params.set("page", newPage.toString());
-      navigate(`?${params.toString()}`, { replace: true });
-    };
-    if (location.search) {
-      updateQueryParam();
-    }
-  }, [table.getState().pagination.pageIndex, navigate, location.search]);
+    setIsTableLoading(isLoading);
+  }, [isLoading]);
 
   return (
     <div className="w-full overflow-auto p-10 absolute top-0 right-0 left-0 bottom-0">
       <div className="flex flex-col items-center xl:flex-row gap-2 py-4">
         <div className="relative flex items-center w-full text-14 rounded-[8px]">
-          <img src={SearchIcon} alt="searchicon" className="absolute left-3 w-6 h-6 text-gray-500" />
+          <img
+            src={SearchIcon}
+            alt="searchicon"
+            className="absolute left-3 w-6 h-6 text-gray-500"
+          />
           <Input
             placeholder="جستجو براساس نام"
-            value={(table.getColumn("first_name")?.getFilterValue() as string) ?? ""}
-            onChange={(event) => {
-              table.setPageIndex(0);
-              table.getColumn("first_name")?.setFilterValue(event.target.value);
-            }}
+            value={firstName}
+            onChange={(e) => handleSearch("first_name", e.target.value)}
             className="text-16 placeholder:text-16 rounded-[8px] text-gray-900 border-slate-400 placeholder:text-gray-500 hover:placeholder:text-blue-500 hover:cursor-pointer"
           />
         </div>
         <div className="relative flex items-center w-full text-14 rounded-[8px]">
-          <img src={SearchIcon} alt="searchicon" className="absolute left-3 w-6 h-6 text-gray-500" />
+          <img
+            src={SearchIcon}
+            alt="searchicon"
+            className="absolute left-3 w-6 h-6 text-gray-500"
+          />
           <Input
             placeholder="جستجو براساس نام خانوادگی"
-            value={(table.getColumn("last_name")?.getFilterValue() as string) ?? ""}
-            onChange={(event) => {
-              table.setPageIndex(0);
-              table.getColumn("last_name")?.setFilterValue(event.target.value);
-            }}
+            value={lastName}
+            onChange={(e) => handleSearch("last_name", e.target.value)}
             className="text-16 placeholder:text-16 rounded-[8px] text-gray-900 border-slate-400 placeholder:text-gray-500 hover:placeholder:text-blue-500 hover:cursor-pointer"
           />
         </div>
         <div className="relative flex items-center w-full text-14 rounded-[8px]">
-          <img src={SearchIcon} alt="searchicon" className="absolute left-3 w-6 h-6 text-gray-500" />
+          <img
+            src={SearchIcon}
+            alt="searchicon"
+            className="absolute left-3 w-6 h-6 text-gray-500"
+          />
           <Input
             placeholder="جستجو براساس رشته"
-            value={(table.getColumn("field")?.getFilterValue() as string) ?? ""}
-            onChange={(event) => {
-              table.setPageIndex(0);
-              table.getColumn("field")?.setFilterValue(event.target.value);
-            }}
+            value={field}
+            onChange={(e) => handleSearch("field", e.target.value)}
             className="text-16 placeholder:text-16 rounded-[8px] text-gray-900 border-slate-400 placeholder:text-gray-500 hover:placeholder:text-blue-500 hover:cursor-pointer"
           />
         </div>
         <div className="relative flex items-center w-full text-14 rounded-[8px]">
-          <img src={SearchIcon} alt="searchicon" className="absolute left-3 w-6 h-6 text-gray-500" />
+          <img
+            src={SearchIcon}
+            alt="searchicon"
+            className="absolute left-3 w-6 h-6 text-gray-500"
+          />
           <Input
             placeholder="جستجو براساس مقطع تحصیلی"
-            value={(table.getColumn("grade")?.getFilterValue() as string) ?? ""}
-            onChange={(event) => {
-              table.setPageIndex(0);
-              table.getColumn("grade")?.setFilterValue(event.target.value);
-            }}
+            value={grade}
+            onChange={(e) => handleSearch("grade", e.target.value)}
             className="text-16 placeholder:text-16 rounded-[8px] text-gray-900 border-slate-400 placeholder:text-gray-500 hover:placeholder:text-blue-500 hover:cursor-pointer"
           />
         </div>
       </div>
       <Table className="!rounded-xl border">
         <TableHeader className="bg-slate-300">
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id} className="!text-center">
-                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-                  </TableHead>
-                );
-              })}
-            </TableRow>
-          ))}
+          <TableRow>
+            {columns.map((col) => (
+              <TableHead key={col.id}>{col.header}</TableHead>
+            ))}
+          </TableRow>
         </TableHeader>
         <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow className="hover:bg-slate-200" key={row.id} data-state={row.getIsSelected() && "selected"}>
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell className="!text-center" key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
+          {isTableLoading ? (
+            <>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <SkeletonRow key={index} columnsCount={columns.length} />
+              ))}
+            </>
+          ) : data.length ? (
+            data.map((row) => (
+              <TableRow key={row.id} className="hover:bg-slate-200">
+                {columns.map((col) => (
+                  <TableCell key={col.id}>{row[col.accessorKey]}</TableCell>
                 ))}
               </TableRow>
             ))
           ) : (
             <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                نتیجه ای یافت نشد...
+              <TableCell colSpan={columns.length} className="text-center">
+                نتیجه‌ای یافت نشد...
               </TableCell>
             </TableRow>
           )}
         </TableBody>
       </Table>
 
-      <div className="flex items-center justify-between py-4 w-full">
-        <div className="flex gap-2">
+      {totalPages && (
+        <div className="flex items-center justify-between py-4">
           <Button
             className="rounded-[8px] border border-black"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
+            onClick={() => handlePageChange(page - 1)}
+            disabled={page <= 1}
           >
             قبلی
           </Button>
+          <span>
+            صفحه {page} از {totalPages}
+          </span>
           <Button
             className="rounded-[8px] border border-black"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
+            onClick={() => handlePageChange(page + 1)}
+            disabled={page >= Number(totalPages)}
           >
             بعدی
           </Button>
         </div>
-        <span className="text-slate-600 text-sm">
-          صفحه {table.getState().pagination.pageIndex + 1} از {table.getPageCount()}
-        </span>
-      </div>
+      )}
     </div>
   );
 }
