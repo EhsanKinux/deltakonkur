73298f4955e96a96918a83cc6c9e2fb1930093a2
Sina Moradian <sinacsharp@gmail.com>
2025-03-12 02:24:56 +0330

    Reformat Pagination: Supervision-Search-By-Name

--- src/components/pages/dashboard/dashboardPages/supervision/tabs/parts/SearchByName.tsx
+++ src/components/pages/dashboard/dashboardPages/supervision/tabs/parts/SearchByName.tsx
@@ -1,14 +1,16 @@
-import { get_students_by_name } from "@/lib/apis/supervision/service";
-import { convertToShamsi } from "@/lib/utils/date/convertDate";
-import { FormEntry } from "../../../advisors/parts/student/table/interfaces";
-import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { SupervisionTable } from "../../table/SupervisionTable";
 import { stColumns } from "../../table/SupervisionColumnDef";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { convertToShamsi } from "@/lib/utils/date/convertDate";
+import { FormEntry } from "../../../advisors/parts/student/table/interfaces";
 import { toast } from "sonner";
 import { useSearchParams } from "react-router-dom";
+import axios from "axios";
+import { BASE_API_URL } from "@/lib/variables/variables";
+import { authStore } from "@/lib/store/authStore";
 
 const SearchByName = () => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -16,39 +18,72 @@ const SearchByName = () => {
     searchParams.get("first_name") || ""
   );
   const [lastName, setLastName] = useState(searchParams.get("last_name") || "");
-  const [students, setStudents] = useState([]);
+  const [students, setStudents] = useState<FormEntry[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [totalPages, setTotalPages] = useState("");
 
-  // Fetch students when search parameters change
-  useEffect(() => {
-    const fetchStudents = async () => {
-      const firstNameParam = searchParams.get("first_name");
-      const lastNameParam = searchParams.get("last_name");
+  const abortControllerRef = useRef<AbortController | null>(null);
 
-      if (firstNameParam || lastNameParam) {
-        try {
-          const data = await get_students_by_name({
-            first_name: firstNameParam || "",
-            last_name: lastNameParam || "",
-          });
+  const activeFname = searchParams.get("first_name");
+  const activeLname = searchParams.get("last_name");
 
-          if (data.length === 0) {
-            toast.warning("دانش‌آموزی با این نام یافت نشد");
-          }
+  // Fetch students based on firstName, lastName, and searchParams
+  const fetchStudents = useCallback(async () => {
+    if (!firstName && !lastName) return;
 
-          const formattedData = data.map((student: FormEntry) => ({
-            ...student,
-            created: convertToShamsi(student.created),
-          }));
+    const { accessToken } = authStore.getState();
 
-          setStudents(formattedData);
-        } catch (error) {
-          console.error("Error fetching students:", error);
-        }
+    // Cancel the previous request if it exists
+    if (abortControllerRef.current) {
+      abortControllerRef.current.abort();
+    }
+    abortControllerRef.current = new AbortController();
+    const signal = abortControllerRef.current.signal;
+
+    try {
+      setIsLoading(true);
+      const { data } = await axios.get(`${BASE_API_URL}api/register/students`, {
+        params: {
+          active: true,
+          first_name: firstName || "",
+          last_name: lastName || "",
+          page: searchParams.get("page") || 1, // Getting the page value from searchParams
+        },
+        signal,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      if (data.results?.length === 0) {
+        toast.warning("دانش‌آموزی با این نام یافت نشد");
       }
-    };
 
+      const formattedData = data.results?.map((student: FormEntry) => ({
+        ...student,
+        created: convertToShamsi(student.created),
+        date_of_birth: student.date_of_birth
+          ? convertToShamsi(student.date_of_birth)
+          : student.date_of_birth,
+      }));
+
+      setStudents(formattedData);
+      setTotalPages(Number(data.count / 10).toFixed(0));
+    } catch (error: unknown) {
+      if (axios.isCancel(error)) {
+        console.log("🔴 درخواست لغو شد");
+      } else {
+        console.error("خطا در دریافت اطلاعات:", error);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  }, [activeFname, activeLname, searchParams]);
+
+  useEffect(() => {
     fetchStudents();
-  }, [searchParams]);
+  }, [fetchStudents]);
 
   const handleSearch = () => {
     if (!firstName && !lastName) {
@@ -56,15 +91,10 @@ const SearchByName = () => {
       return;
     }
 
-    // Merge new search parameters with existing ones
     const newSearchParams = new URLSearchParams(searchParams);
+    newSearchParams.set("page", "1");
     newSearchParams.set("first_name", firstName);
     newSearchParams.set("last_name", lastName);
-
-    setFirstName("");
-    setLastName("");
-
-    // Update the URL search parameters without removing existing params
     setSearchParams(newSearchParams);
   };
 
@@ -76,7 +106,7 @@ const SearchByName = () => {
 
   return (
     <div className="flex flex-col">
-      <div className="flex flex-col-reverse gap-4 xl:flex-row xl:items-end w-full xl:gap-2 p-4 mt-4 rounded-xl shadow-form bg-slate-100">
+      <div className="flex flex-col-reverse xl:items-end w-full gap-2 xl:flex-row p-4 mt-4 rounded-xl shadow-form bg-slate-100">
         <Button
           className="bg-blue-600 text-slate-100 hover:bg-blue-700 hover:text-white rounded-xl pt-2"
           onClick={handleSearch}
@@ -97,7 +127,7 @@ const SearchByName = () => {
             />
           </div>
           <div className="w-full">
-            <Label htmlFor="name">نام خانوادگی:</Label>
+            <Label htmlFor="lastName">نام خانوادگی:</Label>
             <Input
               id="lastName"
               type="text"
@@ -111,7 +141,12 @@ const SearchByName = () => {
       </div>
 
       <div className="flex flex-col justify-center items-center gap-3 p-16 mt-4 shadow-sidebar bg-slate-100 rounded-xl relative min-h-screen">
-        <SupervisionTable columns={stColumns} data={students} />
+        <SupervisionTable
+          columns={stColumns}
+          data={students}
+          isLoading={isLoading}
+          totalPages={totalPages}
+        />
       </div>
     </div>
   );
