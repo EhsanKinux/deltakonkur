a85426b74858e8b72d958ffdd421e08ab6dcfd09
Sina Moradian <sinacsharp@gmail.com>
2025-03-14 23:12:32 +0330

    Fix all students count in main dashboard page

--- src/components/pages/dashboard/dashboard/Dashboard.tsx
+++ src/components/pages/dashboard/dashboard/Dashboard.tsx
@@ -1,17 +1,56 @@
+import profileCover from "@/assets/images/cover-01.png";
 import { useAuth } from "@/lib/apis/authentication/useAuth";
 import { authStore } from "@/lib/store/authStore";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { IUserDetail } from "../dashboardPages/users/userDetail/interface";
-import profileCover from "@/assets/images/cover-01.png";
 // import { getRoleName } from "@/lib/utils/roles/Roles";
 import { useProfile } from "@/functions/hooks/profile/useProfile";
+import { BASE_API_URL } from "@/lib/variables/variables";
+import axios from "axios";
 
 const Dashboard = () => {
+  const [totalStudents, setTotalStudents] = useState(0);
+  const abortControllerRef = useRef<AbortController | null>(null); // اضافه کردن abortController
+
+  const getStudents = useCallback(async () => {
+    const { accessToken } = authStore.getState(); // گرفتن accessToken از authStore
+    // اگر ریکوئست قبلی وجود داشت، کنسل کن
+    if (abortControllerRef.current) {
+      abortControllerRef.current.abort();
+    }
+
+    abortControllerRef.current = new AbortController();
+
+    try {
+      const { data } = await axios.get(
+        `${BASE_API_URL}api/register/students/`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`, // اضافه کردن هدر Authorization
+          },
+        }
+      );
+
+      setTotalStudents(data.count);
+    } catch (error: unknown) {
+      if (axios.isCancel(error)) {
+        console.log("🔴 درخواست لغو شد");
+      } else {
+        console.error("خطا در دریافت اطلاعات دانش آموزان:", error);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    getStudents();
+  }, [getStudents]);
+
   const { userRoles } = authStore();
   const { fetchUserData } = useAuth();
   const [user, setUser] = useState<IUserDetail>();
 
-  const { getCountingStudents, totalActiveStudentsCount, getTotalStudent, totalStudents } = useProfile();
+  const { getCountingStudents, totalActiveStudentsCount } = useProfile();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,14 +62,15 @@ const Dashboard = () => {
 
     fetchData();
     getCountingStudents();
-    getTotalStudent();
-  }, [fetchUserData, userRoles, getCountingStudents, getTotalStudent]);
+  }, [fetchUserData, userRoles, getCountingStudents]);
 
   // const roleName = user ? getRoleName(user?.roles) : "نامشخص";
 
   return (
     <div className="flex flex-col justify-center">
-      <h1 className="border-b-2 border-slate-300 w-fit font-bold text-xl">پروفایل</h1>
+      <h1 className="border-b-2 border-slate-300 w-fit font-bold text-xl">
+        پروفایل
+      </h1>
       <div className="flex flex-col bg-slate-100 rounded-xl shadow-form pb-10 xl:pb-5 gap-8 mt-4">
         <div className="relative z-20 h-35 md:h-65 rounded-xl">
           <img
@@ -44,18 +84,28 @@ const Dashboard = () => {
             {user?.first_name} {user?.last_name}
           </span>
           {/* <span className="text-black text-xl font-light">{roleName}</span> */}
-          <span className="text-black text-nowrap font-thin">شماره همراه : {user?.phone_number}</span>
+          <span className="text-black text-nowrap font-thin">
+            شماره همراه : {user?.phone_number}
+          </span>
         </div>
       </div>
       {userRoles && userRoles.includes(0) && (
         <div className="mt-8 flex justify-center gap-8">
           <div className="flex flex-col items-center bg-blue-100 p-4 rounded-lg shadow-lg">
-            <span className="text-4xl font-bold text-blue-600">{totalStudents}</span>
-            <span className="text-lg font-medium text-blue-600">کل دانش‌آموزان</span>
+            <span className="text-4xl font-bold text-blue-600">
+              {totalStudents}
+            </span>
+            <span className="text-lg font-medium text-blue-600">
+              کل دانش‌آموزان
+            </span>
           </div>
           <div className="flex flex-col items-center bg-green-100 p-4 rounded-lg shadow-lg">
-            <span className="text-4xl font-bold text-green-600">{totalActiveStudentsCount}</span>
-            <span className="text-lg font-medium text-green-600">تمام دانش‌آموزان فعال</span>
+            <span className="text-4xl font-bold text-green-600">
+              {totalActiveStudentsCount}
+            </span>
+            <span className="text-lg font-medium text-green-600">
+              تمام دانش‌آموزان فعال
+            </span>
           </div>
         </div>
       )}
