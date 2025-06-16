4767546042062b24ee0bb854d3391304e0db34e6
sinac0de <sinacsharp@gmail.com>
2025-06-17 00:08:38 +0330

    Add back button to AssessmentDescription.

--- src/components/pages/dashboard/dashboardPages/supervision/assess/parts/recentAssassments/DescriptionPage.tsx
+++ src/components/pages/dashboard/dashboardPages/supervision/assess/parts/recentAssassments/DescriptionPage.tsx
@@ -1,13 +1,26 @@
+import { Button } from "@/components/ui/button";
 import { useLocation } from "react-router-dom";
+import backIcon from "@/assets/icons/back.svg";
 
 export function DescriptionPage() {
   const location = useLocation();
   const { description } = location.state || {};
 
+  const goBack = () => {
+    window.history.go(-1);
+  };
+
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">توضیحات</h1>
-      <p className="text-lg">{description || "توضیحاتی وجود ندارد."}</p>
+    <div className="p-4 flex flex-col gap-2">
+      <Button
+        className="flex gap-1 font-bold text-sm text-slate-600 rounded hover:text-blue-600 justify-start items-center"
+        onClick={goBack}
+      >
+        <img className="w-5 pb-[2px]" src={backIcon} alt="backIcon" />
+        <span>بازگشت به لیست نظرسنجی‌ها</span>
+      </Button>
+      <h1 className="text-2xl font-bold mb-4 text-center">توضیحات</h1>
+      <p className="text-sm ">{description || "توضیحاتی وجود ندارد."}</p>
     </div>
   );
 }
